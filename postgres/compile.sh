75784effed12d7b10d918ed364c6424079057b92
Eric B. Ridge <e_ridge@tcdi.com>
2015-07-19 13:18:28 -0400

    more messin' 'round with travis

--- postgres/compile.sh
+++ postgres/compile.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
 
+echo -n "Where is PG config:  "
+which pg_config
+
+echo "PATH=$PATH"
+echo
+echo
+
+ls -la /usr/lib/postgresql/
+
+echo "pg_config output"
 pg_config
 
-make clean install
+# make clean install
 
 # always return cleanly
 exit 0;
