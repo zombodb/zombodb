44f502d91294b74602669dc92b09c62c5701436b
Eric B. Ridge <e_ridge@tcdi.com>
2015-07-19 13:22:29 -0400

    more messin' 'round with travis

--- postgres/compile.sh
+++ postgres/compile.sh
@@ -2,17 +2,26 @@
 
 echo -n "Where is PG config:  "
 which pg_config
+ls -la `which pg_config`
 
 echo "PATH=$PATH"
 echo
 echo
 
+echo "/usr/lib/postgresql/"
 ls -la /usr/lib/postgresql/
+echo
 
+echo "/usr/lib/postgresql/9.3/bin"
+ls -la /usr/lib/postgresql/9.3/bin
+
+
+export PATH=/usr/lib/postgresql/9.3/bin:$PATH
 echo "pg_config output"
 pg_config
 
-# make clean install
+
+make clean install
 
 # always return cleanly
 exit 0;
