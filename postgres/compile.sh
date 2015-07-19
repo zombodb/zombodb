a9fa9eccd4bf2d077d29126cdc2af4d175ef85c9
Eric B. Ridge <e_ridge@tcdi.com>
2015-07-19 13:56:04 -0400

    remove debugging stuff

--- postgres/compile.sh
+++ postgres/compile.sh
@@ -1,27 +1,4 @@
 #! /bin/bash
 
-echo -n "Where is PG config:  "
-which pg_config
-ls -la `which pg_config`
-
-echo "PATH=$PATH"
-echo
-echo
-
-echo "/usr/lib/postgresql/"
-ls -la /usr/lib/postgresql/
-echo
-
-echo "/usr/lib/postgresql/9.3/bin"
-ls -la /usr/lib/postgresql/9.3/bin
-
-
-export PATH=/usr/lib/postgresql/9.3/bin:$PATH
-echo "pg_config output"
-pg_config
-
-
 make clean install
-
-# always return cleanly
-exit 0;
+exit $?;
