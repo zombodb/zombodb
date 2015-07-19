3b6bf3e68f28a0bd7a7469ba297a0a66fef96d0d
Eric B. Ridge <eebbrr@gmail.com>
2015-07-19 15:23:15 -0400

    deal with differences between linux and os x

--- postgres/compile.sh
+++ postgres/compile.sh
@@ -8,7 +8,12 @@ make clean
 make
 
 WHOAMI=$(id -u)
-PG_OWNER=$(stat -f %u `pg_config --libdir`)
+if [ `uname` == "Darwin" ] ; then
+	PG_OWNER=$(stat -f %u `pg_config --libdir`)
+else
+	PG_OWNER=$(start --format %u `pg_config --libdir`)
+fi
+
 if [ $WHOAMI = $PG_OWNER ] ; then
 	make install
 else
