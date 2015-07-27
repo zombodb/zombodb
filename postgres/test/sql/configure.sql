3344e46a4c2a6f2ee32db3dad265f63a4d263cc3
Eric B. Ridge <eebbrr@gmail.com>
2015-07-27 16:55:50 -0400

    issue #2: more work on unit tests

--- postgres/test/sql/configure.sql
+++ postgres/test/sql/configure.sql
@@ -1,17 +1,9 @@
-CREATE OR REPLACE FUNCTION assert(msg text, expected anyelement, actual anyelement) RETURNS anyelement LANGUAGE plpgsql AS $$
+CREATE OR REPLACE FUNCTION assert(actual int8, expected int8, message text) RETURNS bool LANGUAGE plpgsql AS $$
 BEGIN
-	IF expected IS DISTINCT FROM actual THEN
-		RAISE EXCEPTION '%: expect:%, got:%', msg, expected, actual;
-	END IF;
-	RETURN expected;
-END;
-$$;
+  IF expected IS DISTINCT FROM actual THEN
+    RAISE EXCEPTION 'ASSERT: %: expected=%, actual=%', message, expected, actual;
+  END IF;
 
-CREATE OR REPLACE FUNCTION assert(msg text, expected integer, actual bigint) RETURNS integer LANGUAGE plpgsql AS $$
-BEGIN
-        IF expected::bigint IS DISTINCT FROM actual THEN
-                RAISE EXCEPTION '%: expect:%, got:%', msg, expected, actual;
-        END IF;
-        RETURN expected;
+  RETURN true;
 END;
 $$;
--- postgres/test/sql/updates.sql
+++ postgres/test/sql/updates.sql
@@ -1,20 +1,22 @@
 set datestyle to 'iso, mdy';
 
-SELECT assert('# of posts', 165240, count(*)) FROM so_posts;
+VACUUM so_posts;
+
+SELECT assert(count(*), 165240, '# of posts') FROM so_posts;
 
 BEGIN;
 UPDATE so_posts SET id = id WHERE id IN (SELECT id FROM so_posts ORDER BY random() LIMIT 1000);
-SELECT assert('# of posts', 165240, count(*)) FROM so_posts;
-SELECT assert('estimated count', 1000 + (SELECT count(*) FROM so_posts), zdb_estimate_count('so_posts', ''));
+SELECT assert(count(*), 165240, '# of posts') FROM so_posts;
+SELECT assert(zdb_estimate_count('so_posts', ''), 1000 + (SELECT count(*) FROM so_posts), 'estimated count');
 COMMIT;
 
-SELECT assert('estimated count', (SELECT count(*) FROM so_posts), zdb_estimate_count('so_posts', ''));
+SELECT assert(zdb_estimate_count('so_posts', ''), (SELECT count(*) FROM so_posts), 'estimated count');
 
 BEGIN;
 UPDATE so_posts SET id = id WHERE id IN (SELECT id FROM so_posts ORDER BY random() LIMIT 1000);
-SELECT assert('# of posts', 165240, count(*)) FROM so_posts;
-SELECT assert('estimated count', 1000 + (SELECT count(*) FROM so_posts), zdb_estimate_count('so_posts', ''));
+SELECT assert(count(*), 165240, '# of posts') FROM so_posts;
+SELECT assert(zdb_estimate_count('so_posts', ''), 1000 + (SELECT count(*) FROM so_posts), 'estimated count');
 ABORT;
 
-SELECT assert('estimated count', (SELECT count(*) FROM so_posts), zdb_estimate_count('so_posts', ''));
+SELECT assert(zdb_estimate_count('so_posts', ''), (SELECT count(*) FROM so_posts), 'estimated count');
 
