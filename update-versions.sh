e0332dc685767a8ed0119f18473754c4ba0d8735
Eric B. Ridge <eebbrr@gmail.com>
2015-11-05 14:08:55 -0700

    update .gitignore too

--- update-versions.sh
+++ update-versions.sh
@@ -7,7 +7,7 @@ if [ "${OLD}" == "" ] || [ "${NEW}" == "" ] ; then
 	echo Usage: ./update-versions.sh OLDVER NEWVER
 	exit 1
 fi
-for f in `find . -name pom.xml` postgres/zombodb.control docker/postgres/Dockerfile docker/elasticsearch/Dockerfile ; do
+for f in `find . -name pom.xml` postgres/zombodb.control docker/postgres/Dockerfile docker/elasticsearch/Dockerfile .gitignore ; do
 	echo Processing: $f
 	sed -i.bak s/${OLD}/${NEW}/g $f
 done
@@ -15,3 +15,4 @@ done
 SQL=postgres/src/main/sql/zombodb--${OLD}-${NEW}.sql
 echo "-- no sql changes" > ${SQL}
 git add ${SQL}
+echo ${SQL} >> ./.gitignore
