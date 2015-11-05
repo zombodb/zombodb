ed28c01045e77cd44a86dac727a8e86d0d8cf4fe
Eric B. Ridge <eebbrr@gmail.com>
2015-11-05 14:07:08 -0700

    bump version to 2.5.1_BETA1 and make script to make this easy

---
+++ update-versions.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+OLD=$1
+NEW=$2
+
+if [ "${OLD}" == "" ] || [ "${NEW}" == "" ] ; then
+	echo Usage: ./update-versions.sh OLDVER NEWVER
+	exit 1
+fi
+for f in `find . -name pom.xml` postgres/zombodb.control docker/postgres/Dockerfile docker/elasticsearch/Dockerfile ; do
+	echo Processing: $f
+	sed -i.bak s/${OLD}/${NEW}/g $f
+done
+
+SQL=postgres/src/main/sql/zombodb--${OLD}-${NEW}.sql
+echo "-- no sql changes" > ${SQL}
+git add ${SQL}
