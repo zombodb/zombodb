d2dc1779934dee89db35da93ed97ac036ab2d939
Eric B. Ridge <eebbrr@gmail.com>
2015-12-01 15:45:30 -0700

    fighting with travis again -- wow, do I suck?

--- postgres/src/main/shell/hack-configs-for-travisci.sh
+++ postgres/src/main/shell/hack-configs-for-travisci.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-echo << DONE >> /etc/elasticsearch/elasticsearch.yml
+cat << DONE >> /etc/elasticsearch/elasticsearch.yml
 script.disable_dynamic: false
 threadpool.bulk.queue_size: 1024
 threadpool.bulk.size: 12
@@ -8,7 +8,7 @@ http.max_content_length: 1024mb
 index.query.bool.max_clause_count: 1000000
 DONE
 
-echo << DONE >> /etc/postgresql/9.3/main/postgresql.conf
+cat << DONE >> /etc/postgresql/9.3/main/postgresql.conf
 local_preload_libraries='zombodb'
 client_min_messages=notice
 autovacuum=off
