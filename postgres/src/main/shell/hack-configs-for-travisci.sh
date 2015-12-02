d4f46aa589ff34326c08db56fcb3a4206cce3bfc
Eric B. Ridge <eebbrr@gmail.com>
2015-12-02 14:22:54 -0700

    still working on making travis-ci work correctly

--- postgres/src/main/shell/hack-configs-for-travisci.sh
+++ postgres/src/main/shell/hack-configs-for-travisci.sh
@@ -2,8 +2,8 @@
 
 cat << DONE >> /etc/elasticsearch/elasticsearch.yml
 script.disable_dynamic: false
-threadpool.bulk.queue_size: 1024
-threadpool.bulk.size: 12
+threadpool.bulk.queue_size: 10
+threadpool.bulk.size: 2
 http.max_content_length: 1024mb
 index.query.bool.max_clause_count: 1000000
 DONE
--- postgres/src/test/sql/issue-58.sql
+++ postgres/src/test/sql/issue-58.sql
@@ -1,39 +1,37 @@
-\sf zdb(regclass, tid)
---
--- CREATE TABLE a (
---   id SERIAL8 PRIMARY KEY
--- );
--- CREATE TABLE b (
---   id SERIAL8 PRIMARY KEY
--- );
--- CREATE TABLE c (
---   id SERIAL8 PRIMARY KEY
--- );
---
--- CREATE INDEX idxc ON c USING zombodb(zdb('c', ctid), zdb(C)) WITH (url='http://localhost:9200/');
--- CREATE OR REPLACE FUNCTION public.issue_58_func(table_name REGCLASS, ctid tid)
---   RETURNS tid
--- LANGUAGE C
--- IMMUTABLE STRICT
--- AS '$libdir/plugins/zombodb', 'zdb_table_ref_and_tid';
---
--- CREATE INDEX idxc_shadow ON c USING zombodb(issue_58_func('c', ctid), zdb(C)) WITH (shadow='idxc');
---
--- CREATE VIEW issue_58_view AS
---   SELECT
---     a.id                       AS a_id,
---     b.id                       AS b_id,
---     c.id                       AS c_id,
---     issue_58_func('c', c.ctid) AS zdb
---   FROM a, b, C;
---
--- set enable_seqscan to off;
--- set ENABLE_BITMAPSCAN to off;
--- explain (costs off) select * from issue_58_view where zdb==>'';
--- select assert(zdb_determine_index('issue_58_view')::regclass, 'idxc_shadow'::regclass, 'Found correct index');
---
--- DROP VIEW issue_58_view;
--- DROP TABLE a;
--- DROP TABLE b;
--- DROP TABLE c;
--- DROP FUNCTION issue_58_func( REGCLASS, tid );
+CREATE TABLE a (
+  id SERIAL8 PRIMARY KEY
+);
+CREATE TABLE b (
+  id SERIAL8 PRIMARY KEY
+);
+CREATE TABLE c (
+  id SERIAL8 PRIMARY KEY
+);
+
+CREATE INDEX idxc ON c USING zombodb(zdb('c', ctid), zdb(C)) WITH (url='http://localhost:9200/');
+CREATE OR REPLACE FUNCTION public.issue_58_func(table_name REGCLASS, ctid tid)
+  RETURNS tid
+LANGUAGE C
+IMMUTABLE STRICT
+AS '$libdir/plugins/zombodb', 'zdb_table_ref_and_tid';
+
+CREATE INDEX idxc_shadow ON c USING zombodb(issue_58_func('c', ctid), zdb(C)) WITH (shadow='idxc');
+
+CREATE VIEW issue_58_view AS
+  SELECT
+    a.id                       AS a_id,
+    b.id                       AS b_id,
+    c.id                       AS c_id,
+    issue_58_func('c', c.ctid) AS zdb
+  FROM a, b, C;
+
+set enable_seqscan to off;
+set ENABLE_BITMAPSCAN to off;
+explain (costs off) select * from issue_58_view where zdb==>'';
+select assert(zdb_determine_index('issue_58_view')::regclass, 'idxc_shadow'::regclass, 'Found correct index');
+
+DROP VIEW issue_58_view;
+DROP TABLE a;
+DROP TABLE b;
+DROP TABLE c;
+DROP FUNCTION issue_58_func( REGCLASS, tid );
