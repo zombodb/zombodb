28684ad8f5cc4886d53e3d6ed1039438ac3d4cc2
Eric B. Ridge <eebbrr@gmail.com>
2016-03-09 18:26:28 -0700

    copyright header updates

--- postgres/src/main/c/am/zdbseqscan.h
+++ postgres/src/main/c/am/zdbseqscan.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 ZomboDB, LLC
+ * Copyright 2015-2016 ZomboDB, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
