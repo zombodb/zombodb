2180b519fa5608a32708dd527bc93b749231af65
Eric B. Ridge <eebbrr@gmail.com>
2016-04-08 19:11:18 -0600

    turn on doc_values for the fields we can

--- postgres/src/main/c/am/zdbops.c
+++ postgres/src/main/c/am/zdbops.c
@@ -529,6 +529,7 @@ Datum make_es_mapping(Oid tableRelId, TupleDesc tupdesc, bool isAnonymous)
 			appendStringInfo(result, "\"store\": \"true\",");
 			appendStringInfo(result, "\"include_in_all\": \"false\",");
 			appendStringInfo(result, "\"norms\": {\"enabled\":false},");
+            appendStringInfo(result, "\"fielddata\": {\"format\": \"doc_values\"},");
 			appendStringInfo(result, "\"index\": \"not_analyzed\"");
 
 		}
@@ -540,6 +541,7 @@ Datum make_es_mapping(Oid tableRelId, TupleDesc tupdesc, bool isAnonymous)
 			appendStringInfo(result, "\"store\": \"true\",");
 			appendStringInfo(result, "\"include_in_all\": \"false\",");
 			appendStringInfo(result, "\"norms\": {\"enabled\":false},");
+            appendStringInfo(result, "\"fielddata\": {\"format\": \"doc_values\"},");
 			appendStringInfo(result, "\"index\": \"not_analyzed\"");
 
 		}
@@ -550,6 +552,7 @@ Datum make_es_mapping(Oid tableRelId, TupleDesc tupdesc, bool isAnonymous)
 			appendStringInfo(result, "\"type\": \"float\",");
 			appendStringInfo(result, "\"include_in_all\": \"false\",");
 			appendStringInfo(result, "\"norms\": {\"enabled\":false},");
+            appendStringInfo(result, "\"fielddata\": {\"format\": \"doc_values\"},");
 			appendStringInfo(result, "\"index\": \"not_analyzed\"");
 
 		}
@@ -560,6 +563,7 @@ Datum make_es_mapping(Oid tableRelId, TupleDesc tupdesc, bool isAnonymous)
 			appendStringInfo(result, "\"type\": \"double\",");
 			appendStringInfo(result, "\"include_in_all\": \"false\",");
 			appendStringInfo(result, "\"norms\": {\"enabled\":false},");
+            appendStringInfo(result, "\"fielddata\": {\"format\": \"doc_values\"},");
 			appendStringInfo(result, "\"index\": \"not_analyzed\"");
 
 		}
@@ -570,6 +574,7 @@ Datum make_es_mapping(Oid tableRelId, TupleDesc tupdesc, bool isAnonymous)
 			appendStringInfo(result, "\"type\": \"boolean\",");
 			appendStringInfo(result, "\"include_in_all\": \"false\",");
 			appendStringInfo(result, "\"norms\": {\"enabled\":false},");
+            appendStringInfo(result, "\"fielddata\": {\"format\": \"doc_values\"},");
 			appendStringInfo(result, "\"index\": \"not_analyzed\"");
 
 		}
