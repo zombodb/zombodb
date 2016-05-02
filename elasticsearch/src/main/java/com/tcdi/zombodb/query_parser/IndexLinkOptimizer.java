bd12ae65c02356a3c8a259a183847ee5ed02bd19
Eric B. Ridge <eebbrr@gmail.com>
2016-05-02 16:34:21 -0400

    ensure expansions stay children of ASTNot nodes, in the hopes that'll produce a smaller list of ids

--- elasticsearch/src/main/java/com/tcdi/zombodb/query_parser/IndexLinkOptimizer.java
+++ elasticsearch/src/main/java/com/tcdi/zombodb/query_parser/IndexLinkOptimizer.java
@@ -131,6 +131,8 @@ public class IndexLinkOptimizer {
     private void injectASTExpansionNodes(QueryParserNode root) {
         while (root instanceof ASTExpansion)
             root = ((ASTExpansion) root).getQuery();
+        while (root instanceof ASTNot)
+            root = root.getChild(0);
 
         Set<ASTIndexLink> links = collectIndexLinks(root, new HashSet<ASTIndexLink>());
         if (links.size() == 0)
