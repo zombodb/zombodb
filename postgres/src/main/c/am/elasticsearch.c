0c9e8767190c41c7b598a9693cabeed79b4d3d57
Eric B. Ridge <eebbrr@gmail.com>
2016-10-23 17:48:30 -0600

    teach ZDB's vacuum support to _optimize?only_expunge_deletes=true after a threshold of deleted documents.
    Introduces a new index option called "optimize_after" with a default value of zero.

--- postgres/src/main/c/am/elasticsearch.c
+++ postgres/src/main/c/am/elasticsearch.c
@@ -795,11 +795,21 @@ void elasticsearch_freeSearchResponse(ZDBSearchResponse *searchResponse) {
     pfree(searchResponse);
 }
 
+static uint64 count_deleted_docs(ZDBIndexDescriptor *indexDescriptor) {
+	StringInfo endpoint = makeStringInfo();
+	StringInfo response;
+
+	appendStringInfo(endpoint, "%s/_cat/indices/%s?h=docs.deleted", indexDescriptor->url, indexDescriptor->fullyQualifiedName);
+	response = rest_call("GET", endpoint->data, NULL, indexDescriptor->compressionLevel);
+
+	return (uint64) atoll(response->data);
+}
+
 void elasticsearch_bulkDelete(ZDBIndexDescriptor *indexDescriptor, ItemPointer itemPointers, int nitems) {
-    StringInfo endpoint = makeStringInfo();
-    StringInfo request  = makeStringInfo();
-    StringInfo response;
-    int i;
+	StringInfo endpoint = makeStringInfo();
+	StringInfo request  = makeStringInfo();
+	StringInfo response;
+	int        i;
 
     appendStringInfo(endpoint, "%s/%s/data/_zdbbulk?consistency=default", indexDescriptor->url, indexDescriptor->fullyQualifiedName);
     if (strcmp("-1", indexDescriptor->refreshInterval) == 0) {
@@ -824,6 +834,18 @@ void elasticsearch_bulkDelete(ZDBIndexDescriptor *indexDescriptor, ItemPointer i
         response = rest_call("POST", endpoint->data, request, indexDescriptor->compressionLevel);
         checkForBulkError(response, "delete");
     }
+
+	if (indexDescriptor->optimizeAfter > 0) {
+		uint64 deleted_docs = count_deleted_docs(indexDescriptor);
+
+		if (deleted_docs >=  indexDescriptor->optimizeAfter) {
+			resetStringInfo(endpoint);
+			appendStringInfo(endpoint, "%s/%s/_optimize?only_expunge_deletes=true", indexDescriptor->url, indexDescriptor->fullyQualifiedName);
+
+			elog(LOG, "[zombodb vacuum] expunging deleted docs in %s", indexDescriptor->fullyQualifiedName);
+			rest_call("GET", endpoint->data, NULL, indexDescriptor->compressionLevel);
+		}
+	}
 
     freeStringInfo(endpoint);
     freeStringInfo(request);
