0b1f51cd279d2eb5031327ce90356fd1914c2b73
Eric B. Ridge <eebbrr@gmail.com>
2016-11-02 22:35:54 -0600

    fix some shell scripts to use bash; bump version to _BETA8

--- collect-artifacts.sh
+++ collect-artifacts.sh
@@ -1,11 +1,11 @@
-#! /bin/sh
+#! /bin/bash
 
 mkdir -p target/artifacts
 cp $(find ./ -name "zombodb*.deb") $(find ./ -name "zombodb*.rpm") $(find ./ -name "zombodb*.tgz") $(find ./ -name "zombodb*.zip") target/artifacts
 ls -la target/artifacts
 
 if [ `uname` == 'Linux' ] ; then
-	xdg-open target/artifacts
+	xdg-open target/artifacts > /dev/null 2>&1
 else
 	open target/artifacts
 fi
