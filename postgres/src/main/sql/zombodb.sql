fd46211ecbb8267bb551f8c62aa74e5fc86370d7
Eric B. Ridge <eebbrr@gmail.com>
2017-02-28 10:38:37 -0700

    fix issue #191 and bump version to 3.1.9

--- postgres/src/main/sql/zombodb.sql
+++ postgres/src/main/sql/zombodb.sql
@@ -450,21 +450,21 @@ BEGIN
     SELECT column_name
     INTO pkey_column
     FROM information_schema.key_column_usage
-    WHERE (table_catalog || '.' || table_schema || '.' || information_schema.key_column_usage.table_name)::regclass = real_table_name;
+    WHERE format('%I.%I.%I', table_catalog, table_schema, information_schema.key_column_usage.table_name)::regclass = real_table_name;
   END IF;
 
 
   IF pkey_column IS NULL THEN
     /* just get what we can from the underlying table */
-    EXECUTE format('SELECT row_to_json(x) FROM (SELECT %s, ctid FROM %s) x WHERE ctid = ''%s''',
-                   CASE WHEN field_names IS NOT NULL THEN array_to_string(field_names, ',') ELSE (select array_to_string(array_agg(attname), ',') from pg_attribute where attrelid = real_table_name and atttypid <> 'fulltext'::regtype and not attisdropped and attnum >=0) END,
+    EXECUTE format('SELECT row_to_json(x) FROM (SELECT "%s", ctid FROM %s) x WHERE ctid = ''%s''',
+                   CASE WHEN field_names IS NOT NULL THEN array_to_string(field_names, '","') ELSE (select array_to_string(array_agg(attname), '","') from pg_attribute where attrelid = real_table_name and atttypid <> 'fulltext'::regtype and not attisdropped and attnum >=0) END,
                    real_table_name,
                    row_ctid) INTO row_data;
   ELSE
     /* select out of the view */
-    EXECUTE format('SELECT row_to_json(x) FROM (SELECT %s as _zdb_pkey, %s FROM %s) x WHERE _zdb_pkey = (SELECT %s FROM %s WHERE ctid = ''%s'')',
-                   CASE WHEN field_names IS NOT NULL THEN array_to_string(field_names, ',') ELSE (select array_to_string(array_agg(attname), ',') from pg_attribute where attrelid = table_name and atttypid <> 'fulltext'::regtype and not attisdropped and attnum >=0) END,
+    EXECUTE format('SELECT row_to_json(x) FROM (SELECT %I as _zdb_pkey, "%s" FROM %s) x WHERE _zdb_pkey = (SELECT %I FROM %s WHERE ctid = ''%s'')',
                    pkey_column,
+                   CASE WHEN field_names IS NOT NULL THEN array_to_string(field_names, '","') ELSE (select array_to_string(array_agg(attname), '","') from pg_attribute where attrelid = table_name and atttypid <> 'fulltext'::regtype and not attisdropped and attnum >=0) END,
                    table_name,
                    pkey_column,
                    real_table_name,
