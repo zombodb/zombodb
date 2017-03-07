2c632f30d153df93e8699536ade6c322e5b6559a
taspotts <taspotts@users.noreply.github.com>
2017-03-07 12:07:09 -0500

    added boolean operator single-character abbreviation tests (#194)
    * added some fulltext tests
    
    * added some more fulltext tests
    
    * Update fulltext.sql
    
    * operator tests and fix up some typos
    
    * added some ? wildcard tests for fulltext and shingles
    
    * added boolean operator single-character abbreviation test

--- postgres/src/test/zdbtaptests/tests/null.sql
+++ postgres/src/test/zdbtaptests/tests/null.sql
@@ -2,7 +2,7 @@
 
 -- Start transaction and plan the tests.
 BEGIN;
-SELECT plan(8);
+SELECT plan(12);
 
 -- Run the tests.
 --**********************************************************************************************************************
@@ -22,9 +22,19 @@ PREPARE zdb_result AS SELECT pk_data FROM unit_tests.consolidated_record_view WH
 SELECT set_eq('expected_result', 'zdb_result', 'NOT pk_data:NULL');
 --**********************************************************************************************************************
 DEALLOCATE ALL;
+PREPARE expected_result AS SELECT unnest(ARRAY[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]::BIGINT[]);
+PREPARE zdb_result AS SELECT pk_data FROM unit_tests.consolidated_record_view WHERE zdb ==> '! pk_data:NULL';
+SELECT set_eq('expected_result', 'zdb_result', '! pk_data:NULL');
+--**********************************************************************************************************************
+DEALLOCATE ALL;
 PREPARE expected_result AS SELECT * FROM (SELECT 1 WHERE 1=2)x; --this returns an empty result set
 PREPARE zdb_result AS SELECT pk_data FROM unit_tests.consolidated_record_view WHERE zdb ==> 'NOT pk_data<>NULL';
 SELECT set_eq('expected_result', 'zdb_result', 'NOT pk_data<>NULL');
+--**********************************************************************************************************************
+DEALLOCATE ALL;
+PREPARE expected_result AS SELECT * FROM (SELECT 1 WHERE 1=2)x; --this returns an empty result set
+PREPARE zdb_result AS SELECT pk_data FROM unit_tests.consolidated_record_view WHERE zdb ==> '! pk_data<>NULL';
+SELECT set_eq('expected_result', 'zdb_result', '! pk_data<>NULL');
 --**********************************************************************************************************************
 DEALLOCATE ALL;
 PREPARE expected_result AS VALUES (7::BIGINT);
@@ -42,10 +52,19 @@ PREPARE zdb_result AS SELECT pk_data FROM unit_tests.consolidated_record_view WH
 SELECT set_eq('expected_result', 'zdb_result', 'NOT data_bigint_1:NULL');
 --**********************************************************************************************************************
 DEALLOCATE ALL;
+PREPARE expected_result AS SELECT unnest(ARRAY[1, 2, 3, 4, 5, 6, 8, 9, 10]::BIGINT[]);
+PREPARE zdb_result AS SELECT pk_data FROM unit_tests.consolidated_record_view WHERE zdb ==> '! data_bigint_1:NULL';
+SELECT set_eq('expected_result', 'zdb_result', '! data_bigint_1:NULL');
+--**********************************************************************************************************************
+DEALLOCATE ALL;
 PREPARE expected_result AS VALUES (7::BIGINT);
 PREPARE zdb_result AS SELECT pk_data FROM unit_tests.consolidated_record_view WHERE zdb ==> 'NOT data_bigint_1<>NULL';
 SELECT set_eq('expected_result', 'zdb_result', 'NOT data_bigint_1<>NULL');
-
+--**********************************************************************************************************************
+DEALLOCATE ALL;
+PREPARE expected_result AS VALUES (7::BIGINT);
+PREPARE zdb_result AS SELECT pk_data FROM unit_tests.consolidated_record_view WHERE zdb ==> '! data_bigint_1<>NULL';
+SELECT set_eq('expected_result', 'zdb_result', '! data_bigint_1<>NULL');
 
 
 
