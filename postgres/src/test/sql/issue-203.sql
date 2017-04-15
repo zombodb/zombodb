9a178a9cbe82272ef2567d9d459bd20f391b1380
Eric B. Ridge <eebbrr@gmail.com>
2017-04-15 12:41:46 -0600

    changing issue #203 regression test for ES 2.4

--- postgres/src/test/sql/issue-203.sql
+++ postgres/src/test/sql/issue-203.sql
@@ -1,2 +1,2 @@
-select id, title from so_posts where zdb('so_posts', ctid) ==> '#limit(id asc, 0, 10) java and title:*' order by 1 asc;
-select id, title from so_posts where zdb('so_posts', ctid) ==> '#limit(_score desc, 0, 10) beer and title:*' order by zdb_score('so_posts', ctid) desc;
+select count(*) from so_posts where zdb('so_posts', ctid) ==> '#limit(id asc, 0, 10) java and title:*';
+select count(*) from so_posts where zdb('so_posts', ctid) ==> '#limit(_score desc, 0, 10) beer and title:*';
