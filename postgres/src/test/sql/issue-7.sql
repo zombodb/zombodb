dab9407408dd7390a15d0e1955d375db7fe777c3
Eric B. Ridge <eebbrr@gmail.com>
2017-04-20 21:31:19 -0600

    adjust test for issue #7 to use new zdb_es_direct_request() instead of rest_get()

--- postgres/src/test/sql/issue-7.sql
+++ postgres/src/test/sql/issue-7.sql
@@ -1,11 +1,11 @@
-SELECT assert((rest_get(zdb_get_url('idxso_posts') || zdb_get_index_name('idxso_posts') || '/_settings') ->
+SELECT assert((zdb_es_direct_request('idxso_posts', 'GET', '_settings')::json ->
                (zdb_get_index_name('idxso_posts')) -> 'settings' -> 'index' ->> 'number_of_replicas')::bigint, 1,
               'Default replica count of 1');
 
-SELECT assert((rest_get(zdb_get_url('idxso_users') || zdb_get_index_name('idxso_users') || '/_settings') ->
+SELECT assert((zdb_es_direct_request('idxso_users', 'GET', '_settings')::json ->
                (zdb_get_index_name('idxso_users')) -> 'settings' -> 'index' ->> 'number_of_replicas')::bigint, 1,
               'Default replica count of 1');
 
-SELECT assert((rest_get(zdb_get_url('idxwords') || zdb_get_index_name('idxwords') || '/_settings') ->
+SELECT assert((zdb_es_direct_request('idxwords', 'GET', '_settings')::json ->
                (zdb_get_index_name('idxwords')) -> 'settings' -> 'index' ->> 'number_of_replicas')::bigint, 1,
               'Default replica count of 1');
