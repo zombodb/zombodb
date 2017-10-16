ffa0cadbc80412dd8deeaafc0bba7543199f60ea
Eric B. Ridge <eebbrr@gmail.com>
2017-10-16 15:34:23 -0600

    change back to use PG9.5

--- postgres/src/main/shell/hack-configs-for-travisci.sh
+++ postgres/src/main/shell/hack-configs-for-travisci.sh
@@ -8,7 +8,7 @@ http.max_content_length: 1024mb
 index.query.bool.max_clause_count: 1000000
 DONE
 
-cat << DONE >> /etc/postgresql/9.3/main/postgresql.conf
+cat << DONE >> /etc/postgresql/9.5/main/postgresql.conf
 local_preload_libraries='zombodb.so'
 client_min_messages=notice
 autovacuum=off
@@ -17,6 +17,6 @@ work_mem=64kB
 fsync=off
 DONE
 
-cat << DONE > /etc/postgresql/9.3/main/pg_hba.conf
+cat << DONE > /etc/postgresql/9.5/main/pg_hba.conf
 local   all             all                                     trust
 DONE
