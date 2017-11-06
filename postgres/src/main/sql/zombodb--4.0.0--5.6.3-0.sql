4909f09470e5a1ea46ac4b2352378233a44b5a17
Eric B. Ridge <eebbrr@gmail.com>
2017-11-05 23:46:07 -0700

    add zdb_profile_query() to the upgrade script from v4.0

--- postgres/src/main/sql/zombodb--4.0.0--5.6.3-0.sql
+++ postgres/src/main/sql/zombodb--4.0.0--5.6.3-0.sql
@@ -1,1 +1,5 @@
--- no sql changes
+CREATE OR REPLACE FUNCTION zdb_internal_profile_query(index_oid oid, user_query text) RETURNS text STRICT IMMUTABLE LANGUAGE c AS '$libdir/plugins/zombodb';
+CREATE OR REPLACE FUNCTION zdb_profile_query(table_name regclass, user_query text) RETURNS text STRICT IMMUTABLE LANGUAGE sql AS $$
+  SELECT zdb_internal_profile_query(zdb_determine_index(table_name), user_query);
+$$;
+
