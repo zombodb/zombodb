78c28bb35be6a16400ae2c16937d6e6a8d8a44f9
Eric B. Ridge <eebbrr@gmail.com>
2017-11-06 13:19:15 -0700

    add normalizer stuff to the upgrade script from v4.0.0

--- postgres/src/main/sql/zombodb--4.0.0--5.6.3-0.sql
+++ postgres/src/main/sql/zombodb--4.0.0--5.6.3-0.sql
@@ -3,3 +3,20 @@ CREATE OR REPLACE FUNCTION zdb_profile_query(table_name regclass, user_query tex
   SELECT zdb_internal_profile_query(zdb_determine_index(table_name), user_query);
 $$;
 
+CREATE TABLE zdb_normalizers (
+  name text NOT NULL PRIMARY KEY,
+  definition json NOT NULL,
+  is_default boolean DEFAULT false NOT NULL
+);
+
+SELECT pg_catalog.pg_extension_config_dump('zdb_normalizers', 'WHERE NOT is_default');
+CREATE OR REPLACE FUNCTION zdb_define_normalizer(name text, definition json) RETURNS void LANGUAGE sql VOLATILE STRICT AS $$
+  DELETE FROM zdb_normalizers WHERE name = $1;
+  INSERT INTO zdb_normalizers(name, definition) VALUES ($1, $2);
+$$;
+INSERT INTO zdb_normalizers(name, definition, is_default) VALUES (
+  'exact', '{
+    "type": "custom",
+    "char_filter": [],
+    "filter": ["lowercase"]
+  }', true);
