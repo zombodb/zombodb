6d1577751c932bfa7caa98b71a099d97c18b5d93
Eric B. Ridge <eebbrr@gmail.com>
2017-11-27 18:42:21 -0700

    do same optimizations for integer fields that we do for longs, and generalize code so we don't duplicate it

--- elasticsearch/src/main/java/llc/zombodb/cross_join/CrossJoinQueryRewriteHelper.java
+++ elasticsearch/src/main/java/llc/zombodb/cross_join/CrossJoinQueryRewriteHelper.java
@@ -15,7 +15,6 @@
  */
 package llc.zombodb.cross_join;
 
-import com.carrotsearch.hppc.IntArrayList;
 import llc.zombodb.fast_terms.FastTermsResponse;
 import llc.zombodb.utils.LongIterator;
 import llc.zombodb.utils.NumberArrayLookupMergeSortIterator;
@@ -37,6 +36,75 @@ class CrossJoinQueryRewriteHelper {
         }
     }
 
+    interface RangeAndSetQueryCreator {
+        Query newRangeQuery(String field, long low, long high);
+        Query newSetQuery(String field, LongIterator itr);
+    }
+
+    static class LongRangeAndSetQueryCreator implements RangeAndSetQueryCreator {
+        @Override
+        public Query newRangeQuery(String field, long low, long high) {
+            return LongPoint.newRangeQuery(field, low, high);
+        }
+
+        @Override
+        public Query newSetQuery(String field, LongIterator itr) {
+            final BytesRef encoded = new BytesRef(new byte[Long.BYTES]);
+
+            return new PointInSetQuery(field, 1, Long.BYTES,
+                    new PointInSetQuery.Stream() {
+
+                        @Override
+                        public BytesRef next() {
+                            if (!itr.hasNext()) {
+                                return null;
+                            } else {
+                                LongPoint.encodeDimension(itr.next(), encoded.bytes, 0);
+                                return encoded;
+                            }
+                        }
+                    }) {
+                @Override
+                protected String toString(byte[] value) {
+                    assert value.length == Long.BYTES;
+                    return Long.toString(LongPoint.decodeDimension(value, 0));
+                }
+            };
+        }
+    }
+
+    static class IntRangeAndSetQueryCreator implements RangeAndSetQueryCreator {
+        @Override
+        public Query newRangeQuery(String field, long low, long high) {
+            return IntPoint.newRangeQuery(field, (int) low, (int) high);
+        }
+
+        @Override
+        public Query newSetQuery(String field, LongIterator itr) {
+            final BytesRef encoded = new BytesRef(new byte[Integer.BYTES]);
+
+            return new PointInSetQuery(field, 1, Integer.BYTES,
+                    new PointInSetQuery.Stream() {
+
+                        @Override
+                        public BytesRef next() {
+                            if (!itr.hasNext()) {
+                                return null;
+                            } else {
+                                IntPoint.encodeDimension((int) itr.next(), encoded.bytes, 0);
+                                return encoded;
+                            }
+                        }
+                    }) {
+                @Override
+                protected String toString(byte[] value) {
+                    assert value.length == Integer.BYTES;
+                    return Integer.toString(IntPoint.decodeDimension(value, 0));
+                }
+            };
+        }
+    }
+
 
     static Query rewriteQuery(CrossJoinQuery crossJoin) {
         FastTermsResponse fastTerms = crossJoin.fastTerms;
@@ -51,9 +119,9 @@ class CrossJoinQueryRewriteHelper {
         //
         switch (fastTerms.getDataType()) {
             case INT:
-                return buildIntRangeOrSetQuery(crossJoin.getLeftFieldname(), new NumberArrayLookupMergeSortIterator(fastTerms.getNumberLookup()));
+                return buildRangeOrSetQuery(crossJoin.getLeftFieldname(), new NumberArrayLookupMergeSortIterator(fastTerms.getNumberLookup()), fastTerms.getRanges(), new IntRangeAndSetQueryCreator());
             case LONG:
-                return buildLongRangeOrSetQuery(crossJoin.getLeftFieldname(), new NumberArrayLookupMergeSortIterator(fastTerms.getNumberLookup()), fastTerms.getRanges());
+                return buildRangeOrSetQuery(crossJoin.getLeftFieldname(), new NumberArrayLookupMergeSortIterator(fastTerms.getNumberLookup()), fastTerms.getRanges(), new LongRangeAndSetQueryCreator());
             case STRING: {
                 BooleanQuery.Builder builder = new BooleanQuery.Builder();
                 for (int shardId = 0; shardId < fastTerms.getNumShards(); shardId++) {
@@ -92,58 +160,7 @@ class CrossJoinQueryRewriteHelper {
         }
     }
 
-    private static Query newSetQuery(String field, int count, int... values) {
-        final BytesRef encoded = new BytesRef(new byte[Integer.BYTES]);
-
-        return new PointInSetQuery(field, 1, Integer.BYTES,
-                new PointInSetQuery.Stream() {
-
-                    int upto;
-
-                    @Override
-                    public BytesRef next() {
-                        if (upto == count) {
-                            return null;
-                        } else {
-                            IntPoint.encodeDimension(values[upto], encoded.bytes, 0);
-                            upto++;
-                            return encoded;
-                        }
-                    }
-                }) {
-            @Override
-            protected String toString(byte[] value) {
-                assert value.length == Integer.BYTES;
-                return Integer.toString(IntPoint.decodeDimension(value, 0));
-            }
-        };
-    }
-
-    private static Query newSetQuery(String field, LongIterator itr) {
-        final BytesRef encoded = new BytesRef(new byte[Long.BYTES]);
-
-        return new PointInSetQuery(field, 1, Long.BYTES,
-                new PointInSetQuery.Stream() {
-
-                    @Override
-                    public BytesRef next() {
-                        if (!itr.hasNext()) {
-                            return null;
-                        } else {
-                            LongPoint.encodeDimension(itr.next(), encoded.bytes, 0);
-                            return encoded;
-                        }
-                    }
-                }) {
-            @Override
-            protected String toString(byte[] value) {
-                assert value.length == Long.BYTES;
-                return Long.toString(LongPoint.decodeDimension(value, 0));
-            }
-        };
-    }
-
-    private static Query buildLongRangeOrSetQuery(String field, NumberArrayLookupMergeSortIterator itr, Collection<long[]> ranges) {
+    private static Query buildRangeOrSetQuery(String field, NumberArrayLookupMergeSortIterator itr, Collection<long[]> ranges, RangeAndSetQueryCreator queryCreator) {
         List<Range> rangeList = new ArrayList<>();
 
         for (long[] range : ranges) {
@@ -174,57 +191,11 @@ class CrossJoinQueryRewriteHelper {
 
         List<Query> clauses = new ArrayList<>();
         for (Range range : rangeList) {
-            clauses.add(LongPoint.newRangeQuery(field, range.start, range.end));
+            clauses.add(queryCreator.newRangeQuery(field, range.start, range.end));
         }
 
         if (itr.hasNext()) {
-            clauses.add(newSetQuery(field, itr));
-        }
-
-        return buildQuery(clauses);
-    }
-
-    private static Query buildIntRangeOrSetQuery(String field, NumberArrayLookupMergeSortIterator itr) {
-        IntArrayList points = new IntArrayList();
-        List<Query> clauses = new ArrayList<>();
-
-        while (itr.hasNext()) {
-            int head, tail;    // range bounds, inclusive
-            int next = (int) itr.next();
-
-            head = tail = next;
-            while (itr.hasNext()) {
-                next = (int) itr.next();
-                if (next != tail + 1) {
-                    // we need 'next' for the subsequent iteration
-                    itr.push(next);
-                    break;
-                } else if (tail == next) {
-                    // it's a duplicate value, so we can de-dup it
-                    continue;
-                }
-                tail++;
-            }
-
-            if (head == tail) {
-                // just one value
-                points.add(head);
-            } else if (tail - head == 1) {
-                // just two points, not worth making a range
-                points.add(head);
-                points.add(tail);
-            } else if (clauses.size() >= BooleanQuery.getMaxClauseCount() - 1) {
-                // we have too many range clauses already
-                for (int i = head; i <= tail; i++)
-                    points.add(i);
-            } else {
-                // it's a range
-                clauses.add(IntPoint.newRangeQuery(field, head, tail));
-            }
-        }
-
-        if (points.size() > 0) {
-            clauses.add(newSetQuery(field, points.size(), points.buffer));
+            clauses.add(queryCreator.newSetQuery(field, itr));
         }
 
         return buildQuery(clauses);
