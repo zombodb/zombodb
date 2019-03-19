f51477f4fafe0538564cd9987dfa6f6a199a6513
Eric B. Ridge <eebbrr@gmail.com>
2019-03-18 19:11:39 -0600

    now that we have custom json type conversion, automatically support converting bytea data to json -- as base64 like ES wants

--- src/sql/json-conversion-support.sql
+++ src/sql/json-conversion-support.sql
@@ -28,3 +28,9 @@ $$;
 
 INSERT INTO zdb.type_conversions (typeoid, funcoid, is_default) VALUES ('point'::regtype, 'zdb.point_to_json'::regproc, true);
 INSERT INTO zdb.type_conversions (typeoid, funcoid, is_default) VALUES ('point[]'::regtype, 'zdb.point_array_to_json'::regproc, true);
+
+CREATE OR REPLACE FUNCTION zdb.bytea_to_json(bytea) RETURNS json PARALLEL SAFE IMMUTABLE STRICT LANGUAGE sql AS $$
+  SELECT to_json(encode($1, 'base64'));
+$$;
+
+INSERT INTO zdb.type_conversions (typeoid, funcoid, is_default) VALUES ('bytea'::regtype, 'zdb.bytea_to_json'::regproc, true);
--- src/sql/zombodb--10-1.0.4--10-1.0.5.sql
+++ src/sql/zombodb--10-1.0.4--10-1.0.5.sql
@@ -46,6 +46,11 @@ INSERT INTO zdb.type_conversions (typeoid, funcoid, is_default) VALUES ('point':
 INSERT INTO zdb.type_conversions (typeoid, funcoid, is_default) VALUES ('point[]'::regtype, 'zdb.point_array_to_json'::regproc, true);
 
 
+CREATE OR REPLACE FUNCTION zdb.bytea_to_json(bytea) RETURNS json PARALLEL SAFE IMMUTABLE STRICT LANGUAGE sql AS $$
+  SELECT to_json(encode($1, 'base64'));
+$$;
+
+INSERT INTO zdb.type_conversions (typeoid, funcoid, is_default) VALUES ('bytea'::regtype, 'zdb.bytea_to_json'::regproc, true);
 
 --
 -- postgis-support.sql
