a05eeda3daedfaa3d2adc039c64f48afae4d9f9b
Eric B. Ridge <eebbrr@gmail.com>
2019-03-20 12:04:44 -0600

    fix dsl.join() and improve its test

--- src/sql/join-support.sql
+++ src/sql/join-support.sql
@@ -5,10 +5,10 @@ CREATE OR REPLACE FUNCTION dsl.join(left_field text, index regclass, right_field
 BEGIN
     IF size > 0 THEN
         /* if we have a size limit, then limit to the top matching hits */
-        RETURN dsl.filter(dsl.terms(left_field, VARIADIC (SELECT array_agg(source->>right_field) FROM zdb.top_hits(index, ARRAY[right_field], query, size))));
+        RETURN dsl.bool(dsl.filter(dsl.terms(left_field, VARIADIC coalesce((SELECT array_agg(source->>right_field) FROM zdb.top_hits(index, ARRAY[right_field], query, size)), ARRAY[]::text[]))));
     ELSE
         /* otherwise, return all the matching terms */
-        RETURN dsl.filter(dsl.terms(left_field, VARIADIC zdb.terms_array(index, right_field, query)));
+        RETURN dsl.bool(dsl.filter(dsl.terms(left_field, VARIADIC coalesce(zdb.terms_array(index, right_field, query), ARRAY[]::text[]))));
     END IF;
 END;
 $$;
--- src/test/sql/test-dsl_join.sql
+++ src/test/sql/test-dsl_join.sql
@@ -1,2 +1,7 @@
-SELECT id FROM events WHERE events ==> dsl.sort('id', 'asc', dsl.limit(10, dsl.join('user_id', 'idxusers', 'id', 'id:1'))) order by id limit 10;
-SELECT id FROM events WHERE events ==> dsl.sort('id', 'asc', dsl.limit(10, dsl.join('user_id', 'idxusers', 'id', 'id:1', 10))) order by id limit 10;
+SELECT id FROM events WHERE events ==> dsl.sort('id', 'asc', dsl.limit(10, dsl.join('user_id', 'idxusers', 'id', 'id:255802'))) order by id;
+SELECT id FROM events WHERE events ==> dsl.sort('id', 'asc', dsl.limit(10, dsl.join('user_id', 'idxusers', 'id', 'id:255802', 10))) order by id;
+SELECT id FROM events WHERE events ==> dsl.sort('id', 'asc', dsl.limit(10, dsl.join('user_id', 'idxusers', 'id', 'id:"-1"'))) order by id;
+SELECT id FROM events WHERE events ==> dsl.sort('id', 'asc', dsl.limit(10, dsl.join('user_id', 'idxusers', 'id', 'id:"-1"', 10))) order by id;
+
+SELECT jsonb_pretty(dsl.join('user_id', 'idxusers', 'id', 'id:255802'));
+SELECT jsonb_pretty(dsl.join('user_id', 'idxusers', 'id', 'id:"-1"'));
