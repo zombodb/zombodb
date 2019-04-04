96521e33755b48bc09db7119afb085f7fef59d21
Eric B. Ridge <eebbrr@gmail.com>
2019-04-03 23:13:32 -0600

    follow-up commit to issue #253:  fix use of STRICT in various dsl.xxx() functions, including the dsl.geo_xxx() queries.

--- src/sql/zombodb--10-1.0.5--4.0.sql
+++ src/sql/zombodb--10-1.0.5--4.0.sql
@@ -8,3 +8,31 @@ CREATE OR REPLACE FUNCTION dsl.datetime_range(field text, lt timestamp with time
             )
         )))::zdbquery;
 $$;
+
+CREATE OR REPLACE FUNCTION dsl.offset_limit("offset" bigint, "limit" bigint, query zdbquery) RETURNS zdbquery PARALLEL SAFE IMMUTABLE LANGUAGE sql AS $$
+    SELECT zdb.set_query_property('limit', "limit"::text, zdb.set_query_property('offset', "offset"::text, query));
+$$;
+
+CREATE OR REPLACE FUNCTION dsl.limit("limit" bigint, query zdbquery) RETURNS zdbquery PARALLEL SAFE IMMUTABLE LANGUAGE sql AS $$
+    SELECT zdb.set_query_property('limit', "limit"::text, query);
+$$;
+
+CREATE OR REPLACE FUNCTION dsl.offset("offset" bigint, query zdbquery) RETURNS zdbquery PARALLEL SAFE IMMUTABLE LANGUAGE sql AS $$
+    SELECT zdb.set_query_property('offset', "offset"::text, query);
+$$;
+
+CREATE OR REPLACE FUNCTION dsl.sort(sort_field text, sort_direction dsl.es_sort_directions, query zdbquery) RETURNS zdbquery PARALLEL SAFE IMMUTABLE LANGUAGE sql AS $$
+    SELECT dsl.sort_many(query, dsl.sd(sort_field, sort_direction));
+$$;
+
+CREATE OR REPLACE FUNCTION dsl.geo_shape(field text, geojson_shape json, relation dsl.es_geo_shape_relation) RETURNS zdbquery PARALLEL SAFE IMMUTABLE LANGUAGE sql AS $$
+  SELECT jsonb_strip_nulls(jsonb_build_object('geo_shape', json_build_object(field, json_build_object('shape', geojson_shape, 'relation', relation))))::zdbquery;
+$$;
+
+CREATE OR REPLACE FUNCTION dsl.geo_bounding_box(field text, box box, type dsl.es_geo_bounding_box_type DEFAULT 'memory') RETURNS zdbquery PARALLEL SAFE IMMUTABLE LANGUAGE sql AS $$
+  SELECT jsonb_strip_nulls(jsonb_build_object('geo_bounding_box', json_build_object('type', type, field, json_build_object('left', (box[0])[0], 'top', (box[0])[1], 'right', (box[1])[0], 'bottom', (box[1])[1]))))::zdbquery;
+$$;
+
+CREATE OR REPLACE FUNCTION dsl.geo_polygon(field text, VARIADIC points point[]) RETURNS zdbquery PARALLEL SAFE IMMUTABLE LANGUAGE sql AS $$
+  SELECT jsonb_strip_nulls(jsonb_build_object('geo_polygon', json_build_object(field, json_build_object('points', zdb.point_array_to_json(points)))))::zdbquery;
+$$;
