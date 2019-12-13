942002c3b7e2fe77192a667c85d508d92e31a767
Eric B. Ridge <eebbrr@gmail.com>
2019-12-13 11:53:32 -0700

    check_for_interrupts() support

--- src/rust/pg-bridge/src/lib.rs
+++ src/rust/pg-bridge/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod log;
+pub mod miscadmin;
 pub mod memcxt;
 pub mod pg_guard;
 pub mod stringinfo;
---
+++ src/rust/pg-bridge/src/miscadmin.rs
@@ -0,0 +1,18 @@
+#[macro_export]
+macro_rules! check_for_interrupts {
+    () => {
+        unsafe {
+            extern "C" {
+                pub static InterruptPending: bool;
+            }
+            if InterruptPending {
+                #[pg_guard]
+                extern "C" {
+                    fn ProcessInterrupts();
+                }
+                ProcessInterrupts();
+            }
+        }
+    }
+}
+
