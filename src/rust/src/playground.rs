d85c803436b06b703d9b8985c2e78a9b6ac806fe
Eric B. Ridge <eebbrr@gmail.com>
2019-12-18 22:12:00 -0700

    playground

--- src/rust/src/playground.rs
+++ src/rust/src/playground.rs
@@ -1,10 +1,19 @@
-use pg_bridge::pg_sys::pg11 as pg_sys;
+use pg_bridge::pg_sys::{pg11 as pg_sys, Datum};
 use pg_bridge::*;
 
 #[pg_extern]
-pub unsafe extern "C" fn rust_add_two_numbers(fcinfo: pg_sys::FunctionCallInfo) -> i64 {
-    let a = pg_getarg_i32(&*fcinfo, 0).unwrap();
-    let b = pg_getarg_i32(&*fcinfo, 1).unwrap();
+pub unsafe extern "C" fn rust_add_two_numbers(fcinfo: pg_sys::FunctionCallInfo) -> pg_sys::Datum {
+    let a = pg_getarg_i32(&fcinfo, 0).unwrap();
+    let b = pg_getarg_i32(&fcinfo, 1).unwrap();
 
-    a as i64 + b as i64
+    (a as i64 + b as i64) as Datum
+}
+
+#[pg_extern]
+pub unsafe extern "C" fn rust_test_text(fcinfo: pg_sys::FunctionCallInfo) -> pg_sys::Datum {
+    let s = pg_getarg_text_pp_as_str(&fcinfo, 0).unwrap();
+
+    info!("{}", s);
+
+    rust_str_to_text_p("This is a string") as Datum
 }
