636d37171ad4055018e37539f25f862276f5dd74
Eric B. Ridge <eebbrr@gmail.com>
2020-01-09 01:38:11 -0700

    starting to stub out ZDB rewritten in Rust!

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use pgx::*;
+
+mod indexam;
+
+pg_module_magic!();
+
+#[pg_extern]
+fn version() -> &'static str {
+    "5.0"
+}
+
+mod tests {
+    use pgx::*;
+
+    #[test]
+    fn make_idea_happy() {}
+
+    #[pg_test]
+    fn test_version() {
+        assert_eq!("5.0", crate::version());
+    }
+}
