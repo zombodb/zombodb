7230200e9c3f6f9db53963a734a9d57f3ed58236
Eric B. Ridge <eebbrr@gmail.com>
2020-01-16 11:12:43 -0700

    s/2019/2020/g

--- elasticsearch/src/main/java/llc/zombodb/query_parser/utils/Utils.java
+++ elasticsearch/src/main/java/llc/zombodb/query_parser/utils/Utils.java
@@ -1,6 +1,6 @@
 /*
  * Portions Copyright 2013-2015 Technology Concepts & Design, Inc
- * Portions Copyright 2015-2019 ZomboDB, LLC
+ * Portions Copyright 2015-2020 ZomboDB, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
