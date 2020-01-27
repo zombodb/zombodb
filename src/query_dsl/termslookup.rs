1bd07b67da9045f9f5b4656440c61d54cb5acf79
David Selph <dave.selph@gmail.com>
2020-01-27 12:37:24 -0700

    updated the termslookup.rs file to use serde in the creation of the ZDBQuery Added test to the tile and they pass

--- src/query_dsl/termslookup.rs
+++ src/query_dsl/termslookup.rs
@@ -7,34 +7,95 @@
 mod dsl {
     use crate::zdbquery::ZDBQuery;
     use pgx::*;
-    use serde_json::*;
+    use serde::*;
+    use std::collections::HashMap;
 
-    fn termslookup(
+    #[derive(Serialize)]
+    struct TermsLookup<'a> {
+        index: &'a str,
+        id: &'a str,
+        path: &'a str,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        routing: Option<&'a str>,
+    }
+
+    #[derive(Serialize)]
+    struct Terms<'a> {
+        terms: HashMap<&'a str, TermsLookup<'a>>,
+    }
+
+    #[pg_extern(immutable, parallel_safe)]
+    pub fn terms_lookup(
         field: &str,
         index: &str,
         id: &str,
         path: &str,
         routing: Option<&str>,
     ) -> ZDBQuery {
-        ZDBQuery::new_with_query_dsl(json! {
-        {
-            "terms": {
-                field : {
-                    "index" : index,
-                     "id" : id,
-                     "path" : path,
-                     "routing" : routing
-                    }
-                }
-            }
-        })
+        let mut terms = HashMap::new();
+        terms.insert(
+            field,
+            TermsLookup {
+                index,
+                id,
+                path,
+                routing,
+            },
+        );
+
+        ZDBQuery::new_with_query_dsl(serde_json::to_value(Terms { terms }).unwrap())
     }
 }
 
-mod test {
+mod tests {
     use crate::query_dsl::termslookup::dsl::*;
-    use crate::zdbquery::ZDBQuery;
     use pgx::*;
-    use pgx_tests::*;
     use serde_json::json;
+
+    #[test]
+    fn make_idea_happy() {}
+
+    #[pg_test]
+    fn test_terms_lookup_with_routing() {
+        let zdbquery = terms_lookup(
+            "fieldname",
+            "test value",
+            "42.0",
+            "path.test.example.foo",
+            Some("routing.info"),
+        );
+        let dsl = zdbquery.query_dsl();
+
+        assert!(dsl.is_some());
+        assert_eq!(
+            dsl.unwrap(),
+            &json! {
+                {
+                    "terms": {"fieldname": { "index": "test value", "id": "42.0", "path": "path.test.example.foo", "routing": "routing.info"  }}
+                }
+            }
+        );
+    }
+
+    #[pg_test]
+    fn test_terms_lookup_without_routing() {
+        let zdbquery = terms_lookup(
+            "fieldname",
+            "test value",
+            "42.0",
+            "path.test.example.foo",
+            None,
+        );
+        let dsl = zdbquery.query_dsl();
+
+        assert!(dsl.is_some());
+        assert_eq!(
+            dsl.unwrap(),
+            &json! {
+                {
+                    "terms": {"fieldname": { "index": "test value", "id": "42.0", "path": "path.test.example.foo" }}
+                }
+            }
+        );
+    }
 }
