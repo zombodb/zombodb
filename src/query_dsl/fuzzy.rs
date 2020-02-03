7e5b3801b9e4c35fd4e88828a50fc35d9d25af69
David Selph <dave.selph@gmail.com>
2020-02-03 10:38:16 -0700

    fix dsl.fuzzy (issue #415)

--- src/query_dsl/fuzzy.rs
+++ src/query_dsl/fuzzy.rs
@@ -1,35 +1,54 @@
 mod dsl {
     use crate::zdbquery::ZDBQuery;
     use pgx::*;
+    use serde::*;
     use serde_json::*;
+
+    #[derive(Serialize)]
+    struct Fuzzy<'a> {
+        value: &'a str,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        boost: Option<f32>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        fuzziness: Option<i64>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        prefix_length: Option<i64>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        max_expansions: Option<i64>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        transpositions: Option<bool>,
+    }
 
     #[pg_extern(immutable, parallel_safe)]
     pub fn fuzzy(
         field: &str,
         value: &str,
-        boost: Option<default!(f32, null)>,
-        fuzziness_integer: Option<default!(i64, null)>,
-        prefix_length: Option<default!(i64, null)>,
+        boost: Option<default!(f32, NULL)>,
+        fuzziness: Option<default!(i64, NULL)>,
+        prefix_length: Option<default!(i64, NULL)>,
         max_expansions: Option<default!(i64, 50)>,
-        transpositions: Option<default!(bool, null)>,
+        transpositions: Option<default!(bool, NULL)>,
     ) -> ZDBQuery {
+        let fuzzy_object = Fuzzy {
+            value,
+            boost,
+            fuzziness,
+            prefix_length,
+            max_expansions,
+            transpositions,
+        };
+
         ZDBQuery::new_with_query_dsl(json! {
             {
-            "fuzzy": {
-                  field: {
-                        "value": value ,
-                        "fuzziness": fuzziness_integer,
-                        "max_expansions": max_expansions,
-                        "prefix_length": prefix_length,
-                        "transpositions": transpositions,
-                        "rewrite": "constant_score"
-                        }
-                   }
+                "fuzzy": {
+                    field: fuzzy_object
+                }
             }
         })
     }
 }
 
+#[cfg(any(test, feature = "pg_test"))]
 mod tests {
     use crate::query_dsl::fuzzy::dsl::*;
     use pgx::*;
@@ -58,12 +77,12 @@ mod tests {
                 {
                     "fuzzy": {
                   "field": {
-                        "value": "value" ,
+                        "value": "value",
+                        "boost": 1.0,
                         "fuzziness": 10,
                         "max_expansions": 50,
                         "prefix_length": 50,
                         "transpositions": true,
-                        "rewrite": "constant_score"
                         }
                    }
                 }
