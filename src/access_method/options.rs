4302067397a93751672dfb71cdea411238d2949f
Eric B. Ridge <eebbrr@gmail.com>
2020-02-03 09:53:51 -0700

    more testing and cleanup around issue #558

--- src/access_method/options.rs
+++ src/access_method/options.rs
@@ -1,6 +1,19 @@
+use lazy_static::*;
 use memoffset::*;
 use pgx::*;
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
+
+const DEFAULT_BATCH_SIZE: i32 = 8 * 1024 * 1024;
+const DEFAULT_COMPRESSION_LEVEL: i32 = 1;
+const DEFAULT_SHARDS: i32 = 5;
+const DEFAULT_OPTIMIZE_AFTER: i32 = 0;
+const DEFAULT_URL: &str = "default";
+const DEFAULT_TYPE_NAME: &str = "doc";
+const DEFAULT_REFRESH_INTERVAL: &str = "-1";
+
+lazy_static! {
+    static ref DEFAULT_BULK_CONCURRENCY: i32 = num_cpus::get() as i32;
+}
 
 #[repr(C)]
 pub struct ZDBIndexOptions {
@@ -25,15 +38,22 @@ pub struct ZDBIndexOptions {
 
 #[allow(dead_code)]
 impl ZDBIndexOptions {
-    pub unsafe fn from(relation: &pg_sys::RelationData) -> &Self {
+    pub unsafe fn from(relation: &PgBox<pg_sys::RelationData>) -> PgBox<ZDBIndexOptions> {
         if relation.rd_index.is_null() {
             panic!("relation doesn't represent an index")
         } else if relation.rd_options.is_null() {
-            panic!("no rd_options on index");
+            // use defaults
+            let mut ops = PgBox::<ZDBIndexOptions>::alloc0();
+            ops.compression_level = DEFAULT_COMPRESSION_LEVEL;
+            ops.shards = DEFAULT_SHARDS;
+            ops.replicas = ZDB_DEFAULT_REPLICAS_GUC;
+            ops.bulk_concurrency = *DEFAULT_BULK_CONCURRENCY;
+            ops.batch_size = DEFAULT_BATCH_SIZE;
+            ops.optimize_after = DEFAULT_OPTIMIZE_AFTER;
+            ops
+        } else {
+            PgBox::from_pg(relation.rd_options as *mut ZDBIndexOptions)
         }
-
-        let ops = relation.rd_options;
-        (ops as *mut ZDBIndexOptions).as_ref().unwrap()
     }
 
     pub fn optimize_after(&self) -> i32 {
@@ -64,16 +84,28 @@ impl ZDBIndexOptions {
         self.llapi
     }
 
-    pub fn url(&self) -> Option<&str> {
-        self.get_str(self.url_offset)
+    pub fn url(&self) -> &str {
+        if self.url_offset == 0 {
+            DEFAULT_URL
+        } else {
+            self.get_str(self.url_offset).unwrap()
+        }
     }
 
-    pub fn type_name(&self) -> Option<&str> {
-        self.get_str(self.type_name_offset)
+    pub fn type_name(&self) -> &str {
+        if self.type_name_offset == 0 {
+            DEFAULT_TYPE_NAME
+        } else {
+            self.get_str(self.type_name_offset).unwrap()
+        }
     }
 
-    pub fn refresh_interval(&self) -> Option<&str> {
-        self.get_str(self.refresh_interval_offset)
+    pub fn refresh_interval(&self) -> &str {
+        if self.refresh_interval_offset == 0 {
+            DEFAULT_REFRESH_INTERVAL
+        } else {
+            self.get_str(self.refresh_interval_offset).unwrap()
+        }
     }
 
     pub fn alias(&self) -> Option<&str> {
@@ -85,7 +117,7 @@ impl ZDBIndexOptions {
     }
 
     fn get_str(&self, offset: i32) -> Option<&str> {
-        if offset <= 0 {
+        if offset == 0 {
             None
         } else {
             let opts = self as *const _ as void_ptr as usize;
@@ -231,12 +263,12 @@ pub unsafe fn init() {
     );
     pg_sys::add_string_reloption(RELOPT_KIND_ZDB, CStr::from_bytes_with_nul_unchecked(b"refresh_interval\0").as_ptr(),
                                  CStr::from_bytes_with_nul_unchecked(b"Frequency in which Elasticsearch indexes are refreshed.  Related to ES' index.refresh_interval setting\0").as_ptr(),
-                                 CStr::from_bytes_with_nul_unchecked(b"-1\0").as_ptr(), None);
+                                 CString::new(DEFAULT_REFRESH_INTERVAL).unwrap().as_ptr(), None);
     pg_sys::add_int_reloption(
         RELOPT_KIND_ZDB,
         CStr::from_bytes_with_nul_unchecked(b"shards\0").as_ptr(),
         CStr::from_bytes_with_nul_unchecked(b"The number of shards for the index\0").as_ptr(),
-        5,
+        DEFAULT_SHARDS,
         1,
         32768,
     );
@@ -255,17 +287,17 @@ pub unsafe fn init() {
             b"The maximum number of concurrent _bulk API requests\0",
         )
         .as_ptr(),
-        12,
+        *DEFAULT_BULK_CONCURRENCY,
         1,
-        num_cpus::get().max(1) as i32,
+        num_cpus::get() as i32,
     );
     pg_sys::add_int_reloption(
         RELOPT_KIND_ZDB,
         CStr::from_bytes_with_nul_unchecked(b"batch_size\0").as_ptr(),
         CStr::from_bytes_with_nul_unchecked(b"The size in bytes of batch calls to the _bulk API\0")
             .as_ptr(),
-        1024 * 1024 * 8,
-        1024,
+        DEFAULT_BATCH_SIZE,
+        1,
         (std::i32::MAX / 2) - 1,
     );
     pg_sys::add_int_reloption(
@@ -275,7 +307,7 @@ pub unsafe fn init() {
             b"0-9 value to indicate the level of HTTP compression\0",
         )
         .as_ptr(),
-        1,
+        DEFAULT_COMPRESSION_LEVEL,
         0,
         9,
     );
@@ -303,7 +335,7 @@ pub unsafe fn init() {
             b"After how many deleted docs should ZDB _optimize the ES index during VACUUM?\0",
         )
         .as_ptr(),
-        0,
+        DEFAULT_OPTIMIZE_AFTER,
         0,
         std::i32::MAX,
     );
@@ -320,7 +352,11 @@ pub unsafe fn init() {
 
 #[cfg(any(test, feature = "pg_test"))]
 mod tests {
-    use crate::access_method::options::{validate_url, ZDBIndexOptions};
+    use crate::access_method::options::{
+        validate_url, ZDBIndexOptions, DEFAULT_BATCH_SIZE, DEFAULT_BULK_CONCURRENCY,
+        DEFAULT_COMPRESSION_LEVEL, DEFAULT_OPTIMIZE_AFTER, DEFAULT_REFRESH_INTERVAL,
+        DEFAULT_SHARDS, DEFAULT_TYPE_NAME, DEFAULT_URL, ZDB_DEFAULT_REPLICAS_GUC,
+    };
     use pgx::*;
     use std::ffi::CString;
 
@@ -358,13 +394,48 @@ mod tests {
 
         let index_oid = Spi::get_one::<pg_sys::Oid>("SELECT 'idxtest'::regclass::oid")
             .expect("failed to get SPI result");
-        let indexrel = pg_sys::RelationIdGetRelation(index_oid);
-        let options = ZDBIndexOptions::from(&indexrel.as_ref().unwrap());
-        assert_eq!(options.url().unwrap(), "localhost:9200/");
-        assert_eq!(options.type_name().unwrap(), "test_type_name");
+        let indexrel = PgBox::from_pg(pg_sys::RelationIdGetRelation(index_oid));
+        let options = ZDBIndexOptions::from(&indexrel);
+        assert_eq!(options.url(), "localhost:9200/");
+        assert_eq!(options.type_name(), "test_type_name");
         assert_eq!(options.alias().unwrap(), "test_alias");
         assert_eq!(options.uuid().unwrap(), "test_uuid");
-        assert_eq!(options.refresh_interval().unwrap(), "5s");
-        pg_sys::RelationClose(indexrel);
+        assert_eq!(options.refresh_interval(), "5s");
+        assert_eq!(options.compression_level(), 1);
+        assert_eq!(options.shards(), 5);
+        assert_eq!(options.replicas(), 0);
+        assert_eq!(options.bulk_concurrency(), num_cpus::get() as i32);
+        assert_eq!(options.batch_size(), 8 * 1024 * 1024);
+        assert_eq!(options.optimize_after(), DEFAULT_OPTIMIZE_AFTER);
+        assert_eq!(options.llapi(), false);
+        pg_sys::RelationClose(indexrel.into_pg());
+    }
+
+    #[pg_test]
+    unsafe fn test_index_options_defaults() {
+        Spi::run(
+            "CREATE TABLE test();  
+        CREATE INDEX idxtest 
+                  ON test 
+               USING zombodb ((test.*));",
+        );
+
+        let index_oid = Spi::get_one::<pg_sys::Oid>("SELECT 'idxtest'::regclass::oid")
+            .expect("failed to get SPI result");
+        let indexrel = PgBox::from_pg(pg_sys::RelationIdGetRelation(index_oid));
+        let options = ZDBIndexOptions::from(&indexrel);
+        assert_eq!(options.url(), DEFAULT_URL);
+        assert_eq!(options.type_name(), DEFAULT_TYPE_NAME);
+        assert_eq!(options.alias(), None);
+        assert_eq!(options.uuid(), None);
+        assert_eq!(options.refresh_interval(), DEFAULT_REFRESH_INTERVAL);
+        assert_eq!(options.compression_level(), DEFAULT_COMPRESSION_LEVEL);
+        assert_eq!(options.shards(), DEFAULT_SHARDS);
+        assert_eq!(options.replicas(), ZDB_DEFAULT_REPLICAS_GUC);
+        assert_eq!(options.bulk_concurrency(), *DEFAULT_BULK_CONCURRENCY);
+        assert_eq!(options.batch_size(), DEFAULT_BATCH_SIZE);
+        assert_eq!(options.optimize_after(), DEFAULT_OPTIMIZE_AFTER);
+        assert_eq!(options.llapi(), false);
+        pg_sys::RelationClose(indexrel.into_pg());
     }
 }
