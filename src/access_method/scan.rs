db97f30916a1f3d4985f842bda6079c4c0d559f9
Eric B. Ridge <eebbrr@gmail.com>
2020-03-06 09:48:47 -0700

    cleanup around memory management of scan state.
    now we use PgMemoryContexts::leak_and_drop_on_delete() for both the ZDBScanState and its contained Iterator pointer to ensure these things get properly freed in all situations.

--- src/access_method/scan.rs
+++ src/access_method/scan.rs
@@ -6,7 +6,6 @@ use pgx::*;
 struct ZDBScanState {
     zdbregtype: pg_sys::Oid,
     iterator: *mut SearchResponseIntoIter,
-    abort_receipt: Option<XactCallbackReceipt>,
 }
 
 #[pg_guard]
@@ -18,17 +17,19 @@ pub extern "C" fn ambeginscan(
     info!("ambeginscan");
     let mut scandesc: PgBox<pg_sys::IndexScanDescData> =
         PgBox::from_pg(unsafe { pg_sys::RelationGetIndexScan(index_relation, nkeys, norderbys) });
-    let mut state = PgBox::<ZDBScanState>::alloc0();
-
-    state.zdbregtype = unsafe {
-        direct_function_call::<pg_sys::Oid>(
-            pg_sys::to_regtype,
-            vec!["pg_catalog.zdbquery".into_datum()],
-        )
-        .expect("failed to lookup type oid for pg_catalog.zdbquery")
+    let state = ZDBScanState {
+        zdbregtype: unsafe {
+            direct_function_call::<pg_sys::Oid>(
+                pg_sys::to_regtype,
+                vec!["pg_catalog.zdbquery".into_datum()],
+            )
+            .expect("failed to lookup type oid for pg_catalog.zdbquery")
+        },
+        iterator: std::ptr::null_mut(),
     };
 
-    scandesc.opaque = state.into_pg() as void_mut_ptr;
+    scandesc.opaque =
+        PgMemoryContexts::CurrentMemoryContext.leak_and_drop_on_delete(state) as void_mut_ptr;
 
     scandesc.into_pg()
 }
@@ -46,7 +47,8 @@ pub extern "C" fn amrescan(
         panic!("No ScanKeys provided");
     }
     let scan: PgBox<pg_sys::IndexScanDescData> = PgBox::from_pg(scan);
-    let mut state = PgBox::from_pg(scan.opaque as *mut ZDBScanState);
+    let mut state =
+        unsafe { (scan.opaque as *mut ZDBScanState).as_mut() }.expect("no scandesc state");
     let nkeys = nkeys as usize;
     let keys = unsafe { std::slice::from_raw_parts(keys as *const pg_sys::ScanKeyData, nkeys) };
     let mut query =
@@ -67,23 +69,8 @@ pub extern "C" fn amrescan(
         .execute()
         .expect("failed to execute ES query");
 
-    // we convert the response from above into a Boxed iterator
-    // and then we leak it so we can hold onto a reference to it in case
-    // the transaction aborts
-    let iterator = Box::new(response.into_iter());
-    state.iterator = Box::leak(iterator);
-
-    // and if the transaction does abort, we'll re-box the iterator and then
-    // immediately drop it, ensuring we don't leak anything across transactions
-    let iter_ptr = state.iterator as void_mut_ptr;
-    state.abort_receipt = Some(register_xact_callback(
-        PgXactCallbackEvent::Abort,
-        move || {
-            // drop the iterator
-            let iter = unsafe { Box::from_raw(iter_ptr as *mut SearchResponseIntoIter) };
-            drop(iter);
-        },
-    ));
+    state.iterator =
+        PgMemoryContexts::CurrentMemoryContext.leak_and_drop_on_delete(response.into_iter());
 }
 
 #[pg_guard]
@@ -92,12 +79,12 @@ pub extern "C" fn amgettuple(
     _direction: pg_sys::ScanDirection,
 ) -> bool {
     let mut scan: PgBox<pg_sys::IndexScanDescData> = PgBox::from_pg(scan);
-    let mut state = PgBox::from_pg(scan.opaque as *mut ZDBScanState);
+    let state = unsafe { (scan.opaque as *mut ZDBScanState).as_mut() }.expect("no scandesc state");
 
     // no need to recheck the returned tuples as ZomboDB indices are not lossy
     scan.xs_recheck = false;
 
-    let iter = unsafe { &mut *state.iterator };
+    let iter = unsafe { state.iterator.as_mut() }.expect("no iterator in state");
     match iter.next() {
         Some((_score, ctid)) => {
             #[cfg(any(feature = "pg10", feature = "pg11"))]
@@ -121,18 +108,6 @@ pub extern "C" fn amgetbitmap(_scan: pg_sys::IndexScanDesc, _tbm: *mut pg_sys::T
 }
 
 #[pg_guard]
-pub extern "C" fn amendscan(scan: pg_sys::IndexScanDesc) {
-    let scan: PgBox<pg_sys::IndexScanDescData> = PgBox::from_pg(scan);
-    let mut state = PgBox::from_pg(scan.opaque as *mut ZDBScanState);
-
-    // unregister the abort callback
-    state
-        .abort_receipt
-        .take()
-        .expect("no amscan abort receipt")
-        .unregister_callback();
-
-    // drop the iterator
-    let iter = unsafe { Box::from_raw(state.iterator as *mut SearchResponseIntoIter) };
-    drop(iter);
+pub extern "C" fn amendscan(_scan: pg_sys::IndexScanDesc) {
+    // nothing to do here
 }
