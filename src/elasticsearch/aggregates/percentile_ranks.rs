87ffc32d011504e29f326a7e4e3f573fda8834f3
David Selph <dave.selph@gmail.com>
2020-03-09 14:14:17 -0600

    percentile_ranks.rs Created and tested Closes #512

---
+++ src/elasticsearch/aggregates/percentile_ranks.rs
@@ -0,0 +1,57 @@
+use crate::elasticsearch::Elasticsearch;
+use crate::zdbquery::ZDBQuery;
+use pgx::*;
+use serde::*;
+use serde_json::*;
+
+#[pg_extern(immutable, parallel_safe)]
+fn percentile_ranks(
+    index: PgRelation,
+    field: &str,
+    query: ZDBQuery,
+    values: Json,
+) -> impl std::iter::Iterator<Item = (name!(key, f64), name!(value, f64))> {
+    #[derive(Deserialize, Serialize)]
+    struct Entry {
+        key: f64,
+        value: f64,
+    }
+
+    #[derive(Deserialize, Serialize)]
+    struct PercentileRankAggData {
+        values: Vec<Entry>,
+    }
+
+    #[derive(Serialize)]
+    struct PercentileRanks<'a> {
+        field: &'a str,
+        values: Json,
+        keyed: bool,
+    }
+
+    let elasticsearch = Elasticsearch::new(&index);
+
+    let percentile_ranks = PercentileRanks {
+        field,
+        values,
+        keyed: false,
+    };
+
+    let request = elasticsearch.aggregate::<PercentileRankAggData>(
+        query,
+        json! {
+            {
+                "percentile_ranks": percentile_ranks,
+            }
+        },
+    );
+
+    let result = request
+        .execute()
+        .expect("failed to execute aggregate search");
+
+    result
+        .values
+        .into_iter()
+        .map(|entry| (entry.key, entry.value))
+}
