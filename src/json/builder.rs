f471f67afca00480512b99c258ae153b9966ea4e
Eric B. Ridge <eebbrr@gmail.com>
2020-03-14 00:49:52 -0600

    code cleanup.  shaves quite a bit of time off indexing too

--- src/json/builder.rs
+++ src/json/builder.rs
@@ -3,203 +3,191 @@ use crate::json::utils::escape_json;
 use pgx::{Date, JsonB, Time, TimeWithTimeZone, Timestamp, TimestampWithTimeZone};
 
 #[derive(Debug)]
-pub struct JsonBuilder<'a> {
-    bool: Vec<(&'a str, bool)>,
-    i16: Vec<(&'a str, i16)>,
-    i32: Vec<(&'a str, i32)>,
-    i64: Vec<(&'a str, i64)>,
-    u32: Vec<(&'a str, u32)>,
-    u64: Vec<(&'a str, u64)>,
-    f32: Vec<(&'a str, f32)>,
-    f64: Vec<(&'a str, f64)>,
-    time: Vec<(&'a str, Time)>,
-    time_with_time_zone: Vec<(&'a str, TimeWithTimeZone)>,
-    timestamp: Vec<(&'a str, Timestamp)>,
-    timestamp_with_time_zone: Vec<(&'a str, TimestampWithTimeZone)>,
-    date: Vec<(&'a str, Date)>,
-    string: Vec<(&'a str, String)>,
-    json_string: Vec<(&'a str, pgx::JsonString)>,
-    jsonb: Vec<(&'a str, JsonB)>,
-    json_value: Vec<(&'a str, serde_json::Value)>,
+#[allow(non_camel_case_types)]
+enum JsonBuilderValue {
+    bool(bool),
+    i16(i16),
+    i32(i32),
+    i64(i64),
+    u32(u32),
+    u64(u64),
+    f32(f32),
+    f64(f64),
+    time(Time),
+    time_with_time_zone(TimeWithTimeZone),
+    timestamp(Timestamp),
+    timestamp_with_time_zone(TimestampWithTimeZone),
+    date(Date),
+    string(String),
+    json_string(pgx::JsonString),
+    jsonb(JsonB),
+    json_value(serde_json::Value),
 
-    bool_array: Vec<(&'a str, Vec<Option<bool>>)>,
-    i16_array: Vec<(&'a str, Vec<Option<i16>>)>,
-    i32_array: Vec<(&'a str, Vec<Option<i32>>)>,
-    i64_array: Vec<(&'a str, Vec<Option<i64>>)>,
-    u32_array: Vec<(&'a str, Vec<Option<u32>>)>,
-    f32_array: Vec<(&'a str, Vec<Option<f32>>)>,
-    f64_array: Vec<(&'a str, Vec<Option<f64>>)>,
-    time_array: Vec<(&'a str, Vec<Option<Time>>)>,
-    time_with_time_zone_array: Vec<(&'a str, Vec<Option<TimeWithTimeZone>>)>,
-    timestamp_array: Vec<(&'a str, Vec<Option<Timestamp>>)>,
-    timestamp_with_time_zone_array: Vec<(&'a str, Vec<Option<TimestampWithTimeZone>>)>,
-    date_array: Vec<(&'a str, Vec<Option<Date>>)>,
-    string_array: Vec<(&'a str, Vec<Option<String>>)>,
-    json_string_array: Vec<(&'a str, Vec<Option<pgx::JsonString>>)>,
-    jsonb_array: Vec<(&'a str, Vec<Option<JsonB>>)>,
+    bool_array(Vec<Option<bool>>),
+    i16_array(Vec<Option<i16>>),
+    i32_array(Vec<Option<i32>>),
+    i64_array(Vec<Option<i64>>),
+    u32_array(Vec<Option<u32>>),
+    f32_array(Vec<Option<f32>>),
+    f64_array(Vec<Option<f64>>),
+    time_array(Vec<Option<Time>>),
+    time_with_time_zone_array(Vec<Option<TimeWithTimeZone>>),
+    timestamp_array(Vec<Option<Timestamp>>),
+    timestamp_with_time_zone_array(Vec<Option<TimestampWithTimeZone>>),
+    date_array(Vec<Option<Date>>),
+    string_array(Vec<Option<String>>),
+    json_string_array(Vec<Option<pgx::JsonString>>),
+    jsonb_array(Vec<Option<JsonB>>),
+}
+
+#[derive(Debug)]
+pub struct JsonBuilder<'a> {
+    values: Vec<(&'a str, JsonBuilderValue)>,
 }
 
 impl<'a> JsonBuilder<'a> {
     pub fn new(num_fields: usize) -> Self {
         JsonBuilder {
-            bool: Vec::with_capacity(num_fields),
-            i16: Vec::with_capacity(num_fields),
-            i32: Vec::with_capacity(num_fields),
-            i64: Vec::with_capacity(num_fields),
-            u32: Vec::with_capacity(num_fields),
-            u64: Vec::with_capacity(num_fields),
-            f32: Vec::with_capacity(num_fields),
-            f64: Vec::with_capacity(num_fields),
-            time: Vec::with_capacity(num_fields),
-            time_with_time_zone: Vec::with_capacity(num_fields),
-            timestamp: Vec::with_capacity(num_fields),
-            timestamp_with_time_zone: Vec::with_capacity(num_fields),
-            date: Vec::with_capacity(num_fields),
-            string: Vec::with_capacity(num_fields),
-            json_string: Vec::with_capacity(num_fields),
-            jsonb: Vec::with_capacity(num_fields),
-            json_value: Vec::with_capacity(num_fields),
-            bool_array: Vec::with_capacity(num_fields),
-            i16_array: Vec::with_capacity(num_fields),
-            i32_array: Vec::with_capacity(num_fields),
-            i64_array: Vec::with_capacity(num_fields),
-            u32_array: Vec::with_capacity(num_fields),
-            f32_array: Vec::with_capacity(num_fields),
-            f64_array: Vec::with_capacity(num_fields),
-            time_array: Vec::with_capacity(num_fields),
-            time_with_time_zone_array: Vec::with_capacity(num_fields),
-            timestamp_array: Vec::with_capacity(num_fields),
-            timestamp_with_time_zone_array: Vec::with_capacity(num_fields),
-            date_array: Vec::with_capacity(num_fields),
-            string_array: Vec::with_capacity(num_fields),
-            json_string_array: Vec::with_capacity(num_fields),
-            jsonb_array: Vec::with_capacity(num_fields),
+            values: Vec::with_capacity(num_fields),
         }
     }
 
     #[inline]
     pub fn add_bool(&mut self, attname: &'a str, value: bool) {
-        self.bool.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::bool(value)));
     }
 
     #[inline]
     pub fn add_i16(&mut self, attname: &'a str, value: i16) {
-        self.i16.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::i16(value)));
     }
 
     #[inline]
     pub fn add_i32(&mut self, attname: &'a str, value: i32) {
-        self.i32.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::i32(value)));
     }
 
     #[inline]
     pub fn add_i64(&mut self, attname: &'a str, value: i64) {
-        self.i64.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::i64(value)));
     }
 
     #[inline]
     pub fn add_u32(&mut self, attname: &'a str, value: u32) {
-        self.u32.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::u32(value)));
     }
 
     #[inline]
     pub fn add_u64(&mut self, attname: &'a str, value: u64) {
-        self.u64.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::u64(value)));
     }
 
     #[inline]
     pub fn add_f32(&mut self, attname: &'a str, value: f32) {
-        self.f32.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::f32(value)));
     }
 
     #[inline]
     pub fn add_f64(&mut self, attname: &'a str, value: f64) {
-        self.f64.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::f64(value)));
     }
 
     #[inline]
     pub fn add_time(&mut self, attname: &'a str, value: Time) {
-        self.time.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::time(value)));
     }
 
     #[inline]
     pub fn add_time_with_time_zone(&mut self, attname: &'a str, value: TimeWithTimeZone) {
-        self.time_with_time_zone.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::time_with_time_zone(value)));
     }
 
     #[inline]
     pub fn add_timestamp(&mut self, attname: &'a str, value: Timestamp) {
-        self.timestamp.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::timestamp(value)));
     }
 
     #[inline]
     pub fn add_timestamp_with_time_zone(&mut self, attname: &'a str, value: TimestampWithTimeZone) {
-        self.timestamp_with_time_zone.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::timestamp_with_time_zone(value)));
     }
 
     #[inline]
     pub fn add_date(&mut self, attname: &'a str, value: Date) {
-        self.date.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::date(value)));
     }
 
     #[inline]
     pub fn add_string(&mut self, attname: &'a str, value: String) {
-        self.string.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::string(value)));
     }
 
     #[inline]
     pub fn add_json_string(&mut self, attname: &'a str, value: pgx::JsonString) {
-        self.json_string.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::json_string(value)));
     }
 
     #[inline]
     pub fn add_jsonb(&mut self, attname: &'a str, value: JsonB) {
-        self.jsonb.push((attname, value));
+        self.values.push((attname, JsonBuilderValue::jsonb(value)));
     }
 
     #[inline]
     pub fn add_json_value(&mut self, attname: &'a str, value: serde_json::Value) {
-        self.json_value.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::json_value(value)));
     }
 
     #[inline]
     pub fn add_bool_array(&mut self, attname: &'a str, value: Vec<Option<bool>>) {
-        self.bool_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::bool_array(value)));
     }
 
     #[inline]
     pub fn add_i16_array(&mut self, attname: &'a str, value: Vec<Option<i16>>) {
-        self.i16_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::i16_array(value)));
     }
 
     #[inline]
     pub fn add_i32_array(&mut self, attname: &'a str, value: Vec<Option<i32>>) {
-        self.i32_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::i32_array(value)));
     }
 
     #[inline]
     pub fn add_i64_array(&mut self, attname: &'a str, value: Vec<Option<i64>>) {
-        self.i64_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::i64_array(value)));
     }
 
     #[inline]
     pub fn add_u32_array(&mut self, attname: &'a str, value: Vec<Option<u32>>) {
-        self.u32_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::u32_array(value)));
     }
 
     #[inline]
     pub fn add_f32_array(&mut self, attname: &'a str, value: Vec<Option<f32>>) {
-        self.f32_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::f32_array(value)));
     }
 
     #[inline]
     pub fn add_f64_array(&mut self, attname: &'a str, value: Vec<Option<f64>>) {
-        self.f64_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::f64_array(value)));
     }
 
     #[inline]
     pub fn add_time_array(&mut self, attname: &'a str, value: Vec<Option<Time>>) {
-        self.time_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::time_array(value)));
     }
 
     #[inline]
@@ -208,12 +196,14 @@ impl<'a> JsonBuilder<'a> {
         attname: &'a str,
         value: Vec<Option<TimeWithTimeZone>>,
     ) {
-        self.time_with_time_zone_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::time_with_time_zone_array(value)));
     }
 
     #[inline]
     pub fn add_timestamp_array(&mut self, attname: &'a str, value: Vec<Option<Timestamp>>) {
-        self.timestamp_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::timestamp_array(value)));
     }
 
     #[inline]
@@ -222,92 +212,87 @@ impl<'a> JsonBuilder<'a> {
         attname: &'a str,
         value: Vec<Option<TimestampWithTimeZone>>,
     ) {
-        self.timestamp_with_time_zone_array.push((attname, value));
+        self.values.push((
+            attname,
+            JsonBuilderValue::timestamp_with_time_zone_array(value),
+        ));
     }
 
     #[inline]
     pub fn add_date_array(&mut self, attname: &'a str, value: Vec<Option<Date>>) {
-        self.date_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::date_array(value)));
     }
 
     #[inline]
     pub fn add_string_array(&mut self, attname: &'a str, value: Vec<Option<String>>) {
-        self.string_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::string_array(value)));
     }
 
     #[inline]
     pub fn add_json_string_array(&mut self, attname: &'a str, value: Vec<Option<pgx::JsonString>>) {
-        self.json_string_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::json_string_array(value)));
     }
 
     #[inline]
     pub fn add_jsonb_array(&mut self, attname: &'a str, value: Vec<Option<JsonB>>) {
-        self.jsonb_array.push((attname, value));
+        self.values
+            .push((attname, JsonBuilderValue::jsonb_array(value)));
     }
 
     pub fn build(self) -> String {
-        let mut cnt;
-        let mut json = String::with_capacity(8192);
+        let mut json = String::new();
 
         json.push('{');
-
-        cnt = self.encode(&mut json, &self.bool, 0);
-        cnt = self.encode(&mut json, &self.i16, cnt);
-        cnt = self.encode(&mut json, &self.i32, cnt);
-        cnt = self.encode(&mut json, &self.i64, cnt);
-        cnt = self.encode(&mut json, &self.u32, cnt);
-        cnt = self.encode(&mut json, &self.u64, cnt);
-        cnt = self.encode(&mut json, &self.f32, cnt);
-        cnt = self.encode(&mut json, &self.f64, cnt);
-        cnt = self.encode(&mut json, &self.string, cnt);
-        cnt = self.encode(&mut json, &self.json_string, cnt);
-        cnt = self.encode(&mut json, &self.jsonb, cnt);
-        cnt = self.encode(&mut json, &self.json_value, cnt);
-        cnt = self.encode(&mut json, &self.time, cnt);
-        cnt = self.encode(&mut json, &self.time_with_time_zone, cnt);
-        cnt = self.encode(&mut json, &self.timestamp, cnt);
-        cnt = self.encode(&mut json, &self.timestamp_with_time_zone, cnt);
-        cnt = self.encode(&mut json, &self.date, cnt);
-
-        cnt = self.encode(&mut json, &self.bool_array, cnt);
-        cnt = self.encode(&mut json, &self.i16_array, cnt);
-        cnt = self.encode(&mut json, &self.i32_array, cnt);
-        cnt = self.encode(&mut json, &self.i64_array, cnt);
-        cnt = self.encode(&mut json, &self.u32_array, cnt);
-        cnt = self.encode(&mut json, &self.f32_array, cnt);
-        cnt = self.encode(&mut json, &self.f64_array, cnt);
-        cnt = self.encode(&mut json, &self.time_array, cnt);
-        cnt = self.encode(&mut json, &self.time_with_time_zone_array, cnt);
-        cnt = self.encode(&mut json, &self.timestamp_array, cnt);
-        cnt = self.encode(&mut json, &self.timestamp_with_time_zone_array, cnt);
-        cnt = self.encode(&mut json, &self.date_array, cnt);
-        cnt = self.encode(&mut json, &self.string_array, cnt);
-        cnt = self.encode(&mut json, &self.json_string_array, cnt);
-        self.encode(&mut json, &self.jsonb_array, cnt);
-
-        json.push('}');
-
-        json
-    }
-
-    #[inline]
-    fn encode<T>(&self, json: &mut String, values: &Vec<(&'a str, T)>, mut cnt: usize) -> usize
-    where
-        T: JsonString,
-    {
-        for (attname, value) in values {
-            if cnt > 0 {
+        for (idx, (key, value)) in self.values.into_iter().enumerate() {
+            if idx > 0 {
                 json.push(',');
             }
 
             json.push('"');
-            escape_json(&attname, json);
+            escape_json(key, &mut json);
             json.push('"');
             json.push(':');
-            value.push_json(json);
-            cnt += 1;
-        }
 
-        cnt
+            match value {
+                JsonBuilderValue::bool(v) => v.push_json(&mut json),
+                JsonBuilderValue::i16(v) => v.push_json(&mut json),
+                JsonBuilderValue::i32(v) => v.push_json(&mut json),
+                JsonBuilderValue::i64(v) => v.push_json(&mut json),
+                JsonBuilderValue::u32(v) => v.push_json(&mut json),
+                JsonBuilderValue::u64(v) => v.push_json(&mut json),
+                JsonBuilderValue::f32(v) => v.push_json(&mut json),
+                JsonBuilderValue::f64(v) => v.push_json(&mut json),
+                JsonBuilderValue::time(v) => v.push_json(&mut json),
+                JsonBuilderValue::time_with_time_zone(v) => v.push_json(&mut json),
+                JsonBuilderValue::timestamp(v) => v.push_json(&mut json),
+                JsonBuilderValue::timestamp_with_time_zone(v) => v.push_json(&mut json),
+                JsonBuilderValue::date(v) => v.push_json(&mut json),
+                JsonBuilderValue::string(v) => v.push_json(&mut json),
+                JsonBuilderValue::json_string(v) => v.push_json(&mut json),
+                JsonBuilderValue::jsonb(v) => v.push_json(&mut json),
+                JsonBuilderValue::json_value(v) => v.push_json(&mut json),
+                JsonBuilderValue::bool_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::i16_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::i32_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::i64_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::u32_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::f32_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::f64_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::time_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::time_with_time_zone_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::timestamp_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::timestamp_with_time_zone_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::date_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::string_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::json_string_array(v) => v.push_json(&mut json),
+                JsonBuilderValue::jsonb_array(v) => v.push_json(&mut json),
+            }
+        }
+        json.push('}');
+
+        json
     }
 }
