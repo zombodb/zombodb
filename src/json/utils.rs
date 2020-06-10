e3e1a759b2fad096fd1ce44720f2ae74aae0aaa5
Eric B. Ridge <eebbrr@gmail.com>
2020-06-10 13:34:13 -0600

    remove unnecessary test

--- src/json/utils.rs
+++ src/json/utils.rs
@@ -59,17 +59,4 @@ mod tests {
 
         assert_eq!(into, "\\b\\f");
     }
-
-    #[test]
-    fn test_json_escape_low() {
-        let mut input = [0u8; 32];
-        let mut into = String::new();
-        for i in 0..32 {
-            input[i] = i as u8;
-        }
-        let input = &unsafe { String::from_utf8_unchecked(input.to_vec()) };
-        escape_json(input, &mut into);
-
-        assert_eq!(into, "\\u004848\\u004849\\u004850\\u004851\\u004852\\u004853\\u004854\\u004855\\b\\t\\n\\u004898\\f\\r\\u0048101\\u0048102\\u004948\\u004949\\u004950\\u004951\\u004952\\u004953\\u004954\\u004955\\u004956\\u004957\\u004997\\u004998\\u004999\\u0049100\\u0049101\\u0049102");
-    }
 }
