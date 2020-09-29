82f941f3ac2a3ee85753717f9d1d8894d4a90cdd
Eric B. Ridge <eebbrr@gmail.com>
2020-09-29 09:47:56 -0600

    more data in preparation of porting old es5.6-dev branch tests

--- test/sql/index-data.sql
+++ test/sql/index-data.sql
@@ -1,2 +1,7 @@
 CREATE INDEX idxevents ON events USING zombodb ((events)) WITH (bulk_concurrency=2, batch_size=2097152);
 CREATE INDEX idxusers ON users USING zombodb ((users)) WITH (bulk_concurrency=2, batch_size=2097152);
+CREATE INDEX idxso_users ON so_users USING zombodb ((so_users)) WITH (bulk_concurrency=2, batch_size=2097152);
+CREATE INDEX idxso_posts ON so_posts USING zombodb ((so_posts)) WITH (bulk_concurrency=2, batch_size=2097152);
+CREATE INDEX idxso_comments ON so_comments USING zombodb ((so_comments)) WITH (bulk_concurrency=2, batch_size=2097152);
+CREATE INDEX idxproducts ON products USING zombodb ((products)) WITH (bulk_concurrency=2, batch_size=2097152);
+CREATE INDEX idxwords ON words USING zombodb ((words)) WITH (bulk_concurrency=2, batch_size=2097152);
