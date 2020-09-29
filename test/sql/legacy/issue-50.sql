ea6dc2277a1c82bb46030743699879c6499882d1
David Selph <dave.selph@gmail.com>
2020-09-29 14:44:43 -0600

    update issue #50 test for zdb-rust: NEEDS FIX

--- test/sql/legacy/issue-50.sql
+++ test/sql/legacy/issue-50.sql
@@ -9,10 +9,11 @@ CREATE TABLE issue_50 (
 INSERT INTO issue_50 VALUES (default,
                              'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
                              ARRAY['a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12']::uuid[]);
-CREATE INDEX idxissue_50 ON issue_50 USING zombodb (zdb('issue_50', ctid), zdb(issue_50)) WITH (url='http://localhost:9200/');
 
-SELECT * FROM issue_50 WHERE zdb('issue_50', ctid) ==> 'field1:"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"';
-SELECT * FROM issue_50 WHERE zdb('issue_50', ctid) ==> 'field2:"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"';
-SELECT * FROM issue_50 WHERE zdb('issue_50', ctid) ==> 'field2:"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12"';
+CREATE INDEX idxissue_50 ON issue_50 USING zombodb((issue_50.*));
+
+SELECT * FROM issue_50 WHERE issue_50 ==> 'field1:"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"';
+SELECT * FROM issue_50 WHERE issue_50 ==> 'field2:"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"';
+SELECT * FROM issue_50 WHERE issue_50 ==> 'field2:"a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12"';
 
 ROLLBACK;   /* no need to save this table */
