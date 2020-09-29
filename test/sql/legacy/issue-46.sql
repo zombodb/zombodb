d65404e47c6c930bea4031913d7a18671c23e66c
David Selph <dave.selph@gmail.com>
2020-09-29 13:40:58 -0600

    update issue #46 OR test for zdb-rust Changed function name, Argument order, output column names

--- test/sql/legacy/issue-46.sql
+++ test/sql/legacy/issue-46.sql
@@ -1,1 +1,1 @@
-SELECT * FROM zdb_range_agg('so_posts', 'closed_date', '[ {"from":"2010-12-01T00:00:00-04"}, {"key":"foo", "from":"2010-01-01T00:00:00-04", "to":"2012-12-31T00:00:00-04"}, {"to":"2010-12-14T00:00:00-04"} ]', '') ORDER BY doc_count DESC;
+SELECT * FROM zdb.range('idxso_posts', 'closed_date', '', '[ {"from":"2010-12-01T00:00:00-04"}, {"key":"foo", "from":"2010-01-01T00:00:00-04", "to":"2012-12-31T00:00:00-04"}, {"to":"2010-12-14T00:00:00-04"} ]') ORDER BY doc_count DESC;
