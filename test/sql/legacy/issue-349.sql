9fc55ae41dae4246222ae2666db7b0558f0f1180
Eric B. Ridge <eebbrr@gmail.com>
2020-10-01 12:16:33 -0600

    update issue #349 for zdb-rust

--- test/sql/legacy/issue-349.sql
+++ test/sql/legacy/issue-349.sql
@@ -3,23 +3,23 @@ create table issue349 (
   data text
 );
 
-SELECT zdb_define_filter('issue349_ngram_filter', '{
+SELECT zdb.define_filter('issue349_ngram_filter', '{
   "type": "edgeNGram",
   "min_gram": 3,
   "max_gram": 8
 }');
-SELECT zdb_define_analyzer('issue349_analyzer', '{
+SELECT zdb.define_analyzer('issue349_analyzer', '{
   "type": "custom",
   "tokenizer": "standard",
   "filter": ["lowercase", "asciifolding","issue349_ngram_filter"]
 }');
 
-SELECT zdb_define_mapping('issue349', 'data', '{
+SELECT zdb.define_field_mapping('issue349', 'data', '{
   "store": false,
   "type": "text",
   "fielddata": true,
   "index_options": "positions",
-  "include_in_all": "true",
+  "copy_to": [ "zdb_all" ],
   "analyzer": "issue349_analyzer",
   "fields": {
     "exact": {
@@ -28,13 +28,13 @@ SELECT zdb_define_mapping('issue349', 'data', '{
   }
 }');
 
-create index idxissue349 on issue349 using zombodb (zdb('issue349', ctid), zdb(issue349)) with (url='localhost:9200/');
+create index idxissue349 on issue349 using zombodb ( (issue349.*) );
 
 insert into issue349 (data) values ('jeffrey bob david eric sam california');
 
-select * from issue349 where zdb('issue349', ctid) ==> 'data:jeff';
-select * from issue349 where zdb('issue349', ctid) ==> 'data:j*';
-select * from issue349 where zdb('issue349', ctid) ==> 'data:jeffrey';
-select * from issue349 where zdb('issue349', ctid) ==> 'data:"bob david"';
+select * from issue349 where issue349 ==> 'data:jeff';
+select * from issue349 where issue349 ==> 'data:j*';
+select * from issue349 where issue349 ==> 'data:jeffrey';
+select * from issue349 where issue349 ==> 'data:"bob david"';
 
 drop table issue349;
