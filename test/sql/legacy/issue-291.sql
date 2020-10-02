53ae2da2a3c0ccef140f3e6e3c2db5b79e29eebd
Eric B. Ridge <eebbrr@gmail.com>
2020-10-01 18:34:44 -0600

    update issue #291 for zdb-rust

--- test/sql/legacy/issue-291.sql
+++ test/sql/legacy/issue-291.sql
@@ -1,5 +1,5 @@
 create table issue291 (id serial8 not null primary key, title text);
-create index idxissue292 on issue291 using zombodb (zdb('issue291', ctid), zdb(issue291)) with (url='localhost:9200/');
+create index idxissue292 on issue291 using zombodb ( (issue291.*) );
 insert into issue291 (title) values ('one');
 insert into issue291 (title) values ('two');
 insert into issue291 (title) values ('three');
@@ -11,9 +11,9 @@ insert into issue291 (title) values ('eight');
 insert into issue291 (title) values ('nine');
 insert into issue291 (title) values ('ten');
 
-select * from issue291 where zdb('issue291', ctid) ==> '#limit(title desc, 0, 5)' order by id limit 5;
+select * from issue291 where issue291 ==> dsl.limit(5, dsl.sort('title', 'desc', '')) order by id limit 5;
 
 update issue291 set id = id;
-select * from issue291 where zdb('issue291', ctid) ==> '#limit(title desc, 0, 5)' order by id limit 5;
+select * from issue291 where issue291 ==> dsl.limit(5, dsl.sort('title', 'desc', '')) order by id limit 5;
 
 drop table issue291;
