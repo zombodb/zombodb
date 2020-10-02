be2a1113b79cb04de815bedc3078db770523d342
David Selph <dave.selph@gmail.com>
2020-10-01 19:30:02 -0600

    update issue #80 test for zdb-rust Changed Text type tp VARCHAR, zdb_estimate_count to zdb.count

--- test/sql/legacy/issue-80.sql
+++ test/sql/legacy/issue-80.sql
@@ -1,9 +1,9 @@
 CREATE TABLE tas_test_table
 (
   pk_id bigserial NOT NULL,
-  start_date_text text,
-  end_date_text text,
-  duration text,
+  start_date_text VARCHAR ,
+  end_date_text VARCHAR ,
+  duration VARCHAR ,
   CONSTRAINT tas_test_table_pkey PRIMARY KEY (pk_id)
 );
 
@@ -14,9 +14,9 @@ INSERT INTO tas_test_table VALUES (4, '2/5/2015', '12/31/2016', '695');
 INSERT INTO tas_test_table VALUES (5, '1/1/1999', 'UNKNOWN', NULL);
 INSERT INTO tas_test_table VALUES (6, '2/1/2016', '2/2/2016', '1');
 
-CREATE INDEX idx_tas_test ON tas_test_table USING zombodb(zdb('tas_test_table', ctid), zdb(tas_test_table)) WITH (url='http://localhost:9200/');
+CREATE INDEX idx_tas_test ON tas_test_table USING zombodb((tas_test_table.*));
 
-SELECT assert(zdb_estimate_count('public.tas_test_table', 'end_date_text =[["12/31/1999","2/3/1999", "12/31/2016", "UNKNOWN", "2/2/2016"]]'), 6, 'double-bracket');
-SELECT assert(zdb_estimate_count('public.tas_test_table', 'end_date_text =["12/31/1999","2/3/1999", "12/31/2016", "UNKNOWN", "2/2/2016"]'), 6, 'single-bracket');
+SELECT assert(zdb.count('idx_tas_test', 'end_date_text =[["12/31/1999","2/3/1999", "12/31/2016", "UNKNOWN", "2/2/2016"]]'), 6, 'double-bracket');
+SELECT assert(zdb.count('idx_tas_test', 'end_date_text =["12/31/1999","2/3/1999", "12/31/2016", "UNKNOWN", "2/2/2016"]'), 6, 'single-bracket');
 
 DROP TABLE tas_test_table;
