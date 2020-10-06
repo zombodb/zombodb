f01202cafadf0b462a2739f2d9ea25c56c49997c
David Selph <dave.selph@gmail.com>
2020-10-06 14:16:29 -0600

    update issue #51 test for zdb-rust NEED FIX

--- test/sql/legacy/issue-51.sql
+++ test/sql/legacy/issue-51.sql
@@ -1,2 +1,2 @@
-select * from zdb_tally('so_posts', 'last_activity_date', 'week', 'cheese', 5000, 'term');
-select * from zdb_tally('so_posts', 'last_activity_date', 'week:-1d', 'cheese', 5000, 'term');
+select * from zdb.tally('idxso_posts', 'last_activity_date', 'week', 'cheese', 5000, 'term');
+select * from zdb.tally('idxso_posts', 'last_activity_date', 'week:-1d', 'cheese', 5000, 'term');
