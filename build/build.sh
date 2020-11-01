dc21fa6eebc2a13b98a9075768b19a1b9fc5aa86
Eric B. Ridge <eebbrr@gmail.com>
2020-10-31 18:26:58 -0600

    cleanup Docker build stuff.  All the non-disabled Dockerfiles now build ZDB-Rust.  Getting centos-6/7 to build seems like a task only solved by stacks of cash

--- build/build.sh
+++ build/build.sh
@@ -2,13 +2,75 @@
 
 # set -x
 
-LOGDIR=target/logs
+LOGDIR=`pwd`/target/logs
+REPODIR=`pwd`/target/zombodb
+BRANCH=$1
+IMAGES=$2
+PGVERS=$3
 
-mkdir -p $LOGDIR
-for image in `ls -d zombodb-build-*` ; do
-	echo $image 
+mkdir -p $LOGDIR > /dev/null
 
-	echo "   Building..."
-	docker build -t $image $image 2>&1 > ${LOGDIR}/$image-build.log || exit $?
+if [ "x${BRANCH}" == "x" ] ; then
+	echo "usage:  ./build.sh <branch_name>"
+	exit 1
+fi
 
-done
+if [ "x${IMAGES}" == "x" ] ; then
+	IMAGES=$(ls -d zombodb-build-*)
+fi
+
+if [ "x${PGVERS}" == "x" ] ; then
+	PGVERS="pg10 pg11 pg12"
+fi
+
+function build_zdb {
+	image=$1
+	BUILDDIR=$2
+	LOGDIR=$3
+	REPODIR=$4
+	PGVER=$5
+
+	echo "${image}-${PGVER}:  Building docker image..."
+        docker build -t $1 $1 > ${LOGDIR}/${image}-${PGVER}-build.log 2>&1 || exit $?
+
+	echo "${image}-${PGVER}:  Copying ZomboDB code"
+	rm -rf ${BUILDDIR} > /dev/null
+	mkdir ${BUILDDIR}
+	cp -Rp ${REPODIR} ${BUILDDIR}
+
+	echo "${image}-${PGVER}:  Updating cargo-pgx"
+	docker run -t ${image} cargo install cargo-pgx > ${LOGDIR}/${image}-${PGVER}-cargo-install-cargo-pgx.log 2>&1 || exit $? 
+
+	echo "${image}-${PGVER}:  Building ZomboDB"
+	docker run \
+		-e pgver=${PGVER} \
+		-w /build/zombodb \
+		-v ${BUILDDIR}:/build \
+		-t ${image} \
+		bash -c \
+			'PATH=$(dirname $(cat ~/.pgx/config.toml | grep $pgver | cut -f2 -d= | cut -f2 -d\")):$PATH; \
+			PGX_BUILD_VERBOSE=true;\
+			cargo pgx package' > ${LOGDIR}/${image}-${PGVER}-cargo-pgx-package.log 2>&1 || exit $?
+
+	echo "${image}-${PGVER}:  finished"
+}
+
+export -f build_zdb
+
+echo "Cloning ZomboDB's ${BRANCH} branch"
+rm -rf ${REPODIR} > /dev/null
+git clone \
+	--depth 1 \
+	--single-branch \
+	--branch ${BRANCH} \
+	https://github.com/zombodb/zombodb.git \
+	${REPODIR} > ${LOGDIR}/git-clone.log 2>&1 || exit $? 
+
+for image in ${IMAGES}; do
+	for pgver in ${PGVERS}; do
+		BUILDDIR=`pwd`/target/build/${image}-${pgver}
+
+        	mkdir -p ${BUILDDIR} > /dev/null || exit 1
+		printf "%s\0%s\0%s\0%s\0%s\0" "${image}" "${BUILDDIR}" "${LOGDIR}" "${REPODIR}" "${pgver}"
+	done
+done | xargs -0 -n 5 -P 64 bash -c 'build_zdb "$@"' --
