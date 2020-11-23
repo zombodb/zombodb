9afdd5a0d44c4a9d8c75616b229a6e973f7b520f
David Selph <dave.selph@gmail.com>
2020-11-23 15:41:20 -0700

    Fix/update issue #66

--- test/sql/legacy/issue-66.sql
+++ test/sql/legacy/issue-66.sql
@@ -1,14 +1,14 @@
 CREATE SCHEMA issue_66;
 CREATE TABLE issue_66.test(id serial8 not null primary key);
-CREATE INDEX idxissue_66_test ON issue_66.test USING zombodb (zdb('issue_66.test', ctid), zdb(test)) WITH (url='http://localhost:9200/');
+CREATE INDEX idxissue_66_test ON issue_66.test USING zombodb ((issue_66.test.*));
 
 CREATE SCHEMA issue_66_other;
 CREATE TABLE issue_66_other.test(id serial8 not null primary key);
-CREATE INDEX idxissue_66_test ON issue_66_other.test USING zombodb (zdb('issue_66_other.test', ctid), zdb(test)) WITH (url='http://localhost:9200/');
+CREATE INDEX idxissue_66_test ON issue_66_other.test USING zombodb ((issue_66_other.test.*));
 
 
-SELECT zdb_determine_index('issue_66.test')::regclass = 'issue_66.idxissue_66_test'::regclass;
-SELECT zdb_determine_index('issue_66_other.test')::regclass = 'issue_66_other.idxissue_66_test'::regclass;
+SELECT zdb.determine_index('issue_66.test')::regclass = 'issue_66.idxissue_66_test'::regclass;
+SELECT zdb.determine_index('issue_66_other.test')::regclass = 'issue_66_other.idxissue_66_test'::regclass;
 
 
 DROP SCHEMA issue_66 CASCADE;
