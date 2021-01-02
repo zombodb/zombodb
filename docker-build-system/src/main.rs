eb6c3378d791c4f7669dc07dec6f533a9de4b624
Eric B. Ridge <eebbrr@gmail.com>
2021-01-01 17:20:05 -0700

    change a color

--- docker-build-system/src/main.rs
+++ docker-build-system/src/main.rs
@@ -89,12 +89,7 @@ fn main() -> Result<(), std::io::Error> {
                     image.bold().red(),
                     pgver.to_string().bold().red()
                 );
-                println!(
-                    "{} {} in {:?}",
-                    "       Built".bold().green(),
-                    image,
-                    start.elapsed()
-                );
+                println!("{} {} in {:?}", "       Built", image, start.elapsed());
 
                 let start = std::time::Instant::now();
                 handle_result!(
