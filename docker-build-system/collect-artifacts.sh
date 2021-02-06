ec852a0bdbd4afdadef0fe2a407b0b9237ff4e4e
Eric B. Ridge <eebbrr@gmail.com>
2021-02-06 20:33:53 +0000

    don't need this script anymore

--- docker-build-system/collect-artifacts.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-mkdir -p target/artifacts
-for f in $(find target -name "*.deb") $(find target -name "*.rpm") $(find target -name "*.apk"); do
-	cp $f target/artifacts
-done
-
-[[ $(uname) == "Darwin" ]] && open target/artifacts
