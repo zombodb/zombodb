2ec9ac41c647ca11d7dbf2879e1cbe683f5a75fc
Eric B. Ridge <eebbrr@gmail.com>
2021-08-13 12:12:44 -0600

    fixing issue #673

---
+++ test/sql/issue-673.sql
@@ -0,0 +1,4 @@
+select count(*) from so_posts where so_posts ==> 'body:(the w/3 JAVA)';
+select count(*) from so_posts where so_posts ==> 'body:(the w/3 JAVA*)';
+select count(*) from so_posts where so_posts ==> 'body:(the w/3 JA?A)';
+select count(*) from so_posts where so_posts ==> 'body:(the w/3 JAVA~22)';
