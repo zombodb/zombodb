c0d752b49b2d838352b42681455f0ede4109892e
Eric B. Ridge <eebbrr@gmail.com>
2022-06-30 17:33:39 -0400

    fix upgrade script

--- sql/zombodb--3000.0.4--3000.0.5.sql
+++ sql/zombodb--3000.0.4--3000.0.5.sql
@@ -1,4 +1,3 @@
-=======
 DROP FUNCTION IF EXISTS zdb.schema_version() CASCADE;
 -- it's imperative for `update-versions.sh` that this function be formatted exactly this way
 CREATE FUNCTION zdb.schema_version() RETURNS text LANGUAGE sql AS $$
