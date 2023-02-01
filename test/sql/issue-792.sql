6f221dc8ad0c12b6d4c8c69933b11571820000c8
Eric B. Ridge <eebbrr@gmail.com>
2023-02-01 11:13:32 -0500

    fix issue #792 - highlighting proximity chains

--- test/sql/issue-792.sql
+++ test/sql/issue-792.sql
@@ -8,11 +8,21 @@ SELECT * FROM issue792 WHERE issue792 ==> 't: ( "film" w/7 ("movies" w/7 "barbar
 -- The term "film" is not within 7 of "barbarian" but should highlight as part of nested group
 WITH highlights AS MATERIALIZED (SELECT (
     zdb.highlight_document('issue792'::regclass, json_build_object('t',t), 't: ( "film" w/7 ("movies" w/7 "barbarian"))'::TEXT)).* FROM issue792)
-SELECT * FROM highlights;
+SELECT * FROM highlights order by position;
+
+-- The term "film" is not within 7 of "barbarian" and "queen" but should highlight as part of nested group
+WITH highlights AS MATERIALIZED (SELECT (
+    zdb.highlight_document('issue792'::regclass, json_build_object('t',t), 't: ( "film" w/7 ("movies" w/7 ("barbarian", "queen")))'::TEXT)).* FROM issue792)
+SELECT * FROM highlights order by position;
+
+-- The term "movie" is within 6 of "barbarian" but not "queen" -- should not highlight "queen"
+WITH highlights AS MATERIALIZED (SELECT (
+    zdb.highlight_document('issue792'::regclass, json_build_object('t',t), 't: ( "film" w/7 ("movies" w/6 ("barbarian", "queen")))'::TEXT)).* FROM issue792)
+SELECT * FROM highlights order by position;
 
 -- Adjusted so that term "film" is within 25 of nested group and all three terms are highlighted
 WITH highlights AS MATERIALIZED (SELECT (
     zdb.highlight_document('issue792'::regclass, json_build_object('t',t), 't: ( "film" w/25 ("movies" w/7 "barbarian"))'::TEXT)).* FROM issue792)
-SELECT * FROM highlights;
+SELECT * FROM highlights order by position;
 
 DROP TABLE issue792;
