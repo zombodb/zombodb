eb00c307d0c94c012292c523be965ace4bd65d54
Eric Ridge <eebbrr@gmail.com>
2023-03-30 13:11:43 -0400

    fix issue #821 - we've been formatting the `session_preload_libraries` GUC incorrectly (#824)
    fix issue #821 - we've been formatting the `session_preload_libraries` GUC incorrectly for all these years.  Which can cause problems if there is an existing value.

--- sql/_bootstrap.sql
+++ sql/_bootstrap.sql
@@ -5,12 +5,12 @@ DO LANGUAGE plpgsql $$
         session_preload_libraries = COALESCE (current_setting('session_preload_libraries'), '');
         IF (session_preload_libraries NOT LIKE '%zombodb.so%') THEN
             IF (session_preload_libraries = '') THEN
-                session_preload_libraries = 'zombodb.so';
+                session_preload_libraries = '"zombodb.so"';
             ELSE
-                session_preload_libraries = format('zombodb.so,%s', session_preload_libraries);
+                session_preload_libraries = format('"zombodb.so",%s', session_preload_libraries);
             END IF;
 
-            EXECUTE format('ALTER DATABASE %I SET session_preload_libraries TO ''%s''', current_database(), session_preload_libraries);
+            EXECUTE format('ALTER DATABASE %I SET session_preload_libraries TO %s', current_database(), session_preload_libraries);
         END IF;
 
     END;
