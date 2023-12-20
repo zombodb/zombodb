39c6968613a63c52bf03c635d1d8b8da37d5ca17
Eric Ridge <eebbrr@gmail.com>
2023-12-20 10:06:08 -0500

    Follow up fix for #807 (#883)
    This fixes a problem related to the work done in #807 where ZDB would
    panic with `ERROR:  called Option::unwrap() on a None value` when trying
    to highlight a NULL value using one of the range operators like `>=` or
    `<=`.

--- test/sql/issue-807.sql
+++ test/sql/issue-807.sql
@@ -1,20 +1,22 @@
 CREATE TABLE issue807 (
     id SERIAL8 NOT NULL PRIMARY KEY,
     name text NOT NULL,
-    testdate date
+    testdate date,
+    testdate_array date[]
 );
 CREATE INDEX idxissue807 ON issue807 USING zombodb ((issue807.*));
 
 INSERT INTO issue807(name, testdate)
 SELECT 'testrow'||generate_series(1, 100, 1),
        (NOW() - '1 day'::INTERVAL * ROUND(RANDOM() * 100))::date;
-
+INSERT INTO issue807(name, testdate) VALUES ('null date', NULL);
+INSERT INTO issue807(name, testdate_array) VALUES ('date with null', ARRAY['2022-12-01', NULL]::date[]);
 SELECT count(*)
 FROM issue807
          INNER JOIN LATERAL
     zdb.highlight_document('issue807'::regclass,
                            to_json(issue807),
-                           'testdate >= "2022-12-01"'::TEXT) AS highlights ON true;
+                           'testdate >= "2022-12-01" or testdate_array >= "2022-12-01"'::TEXT) AS highlights ON true;
 
 
 drop table issue807 cascade;
